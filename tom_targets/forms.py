5ea46966b11dbee29cf3dda96847a2bce03b69bf
David Collom <dmcollom@gmail.com>
2018-06-07 10:32:09 -0700

    added forms for sidereal and non-sidereal targets

--- tom_targets/forms.py
+++ tom_targets/forms.py
@@ -1,4 +1,4 @@
-from django.forms
+from django.forms import ModelForm
 from .models import Target
 
 class SiderealTargetCreateForm(ModelForm):
--- tom_targets/views.py
+++ tom_targets/views.py
@@ -3,6 +3,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import Target, TargetList
+from .forms import SiderealTargetCreateForm, NonSiderealTargetCreateForm
 
 
 class TargetList(ListView):
@@ -12,6 +13,13 @@ class TargetList(ListView):
 class TargetCreate(CreateView):
     model = Target
     fields = '__all__'
+
+    def get_form_class(self):
+        if self.kwargs['type'] == 'sidereal':
+            return SiderealTargetCreateForm()
+        elif self.kwargs['type'] ==  'non_sidereal':
+            return NonSiderealTargetCreateForm()
+        return self.form_class
 
 
 class TargetUpdate(UpdateView):
