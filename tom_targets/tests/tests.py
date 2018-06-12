5ed98d57eac678a26c786893d71fcd1e030a7427
David Collom <dcollom@lco.global>
2018-06-12 13:45:14 -0700

    added unit tests

--- tom_targets/tests/tests.py
+++ tom_targets/tests/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.conf import settings
 
 from .factories import SiderealTargetFactory, NonSiderealTargetFactory
 
@@ -16,3 +17,11 @@ class TestTargetDetail(TestCase):
     def test_non_sidereal_target_detail(self):
         response = self.client.get(reverse('targets:detail', kwargs={'pk': self.nst.id}))
         self.assertContains(response, self.nst.id)
+
+    def test_sidereal_target_create(self):
+        response = self.client.get(reverse('targets:create'))
+        self.assertContains(response, settings.SIDEREAL)
+
+    def test_non_sidereal_target_create(self):
+        response = self.client.get(reverse('targets:create'))
+        self.assertContains(response, settings.NON_SIDEREAL)
