e7e6f23d2db365d3cd56dbfb186523b0fe0c6599
Austin Riba <ariba@lco.global>
2018-09-05 11:56:36 -0700

    Add key value model for Target
    Adds a model TargetExtra with a FK to Target so that arbitrary key
    value pairs can be associated with any Target. Adds basic support to the
    Create and Update views and templates for dealing with the formsets
    created for this model.

--- tom_targets/views.py
+++ tom_targets/views.py
@@ -2,10 +2,11 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django_filters.views import FilterView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from django.conf import settings
 
 from .models import Target
-from .forms import SiderealTargetCreateForm, NonSiderealTargetCreateForm
+from .forms import SiderealTargetCreateForm, NonSiderealTargetCreateForm, TargetExtraFormset
 
 
 class TargetListView(FilterView):
@@ -31,6 +32,7 @@ class TargetCreate(CreateView):
     def get_context_data(self, **kwargs):
         context = super(TargetCreate, self).get_context_data(**kwargs)
         context['type_choices'] = Target.TARGET_TYPES
+        context['extra_form'] = TargetExtraFormset()
         return context
 
     def get_form_class(self):
@@ -46,10 +48,30 @@ class TargetCreate(CreateView):
             self.initial['type'] = Target.NON_SIDEREAL
             return NonSiderealTargetCreateForm
 
+    def form_valid(self, form):
+        super().form_valid(form)
+        extra = TargetExtraFormset(self.request.POST)
+        if extra.is_valid():
+            extra.instance = self.object
+            extra.save()
+        return redirect(self.get_success_url())
+
 
 class TargetUpdate(UpdateView):
     model = Target
     fields = '__all__'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['extra_form'] = TargetExtraFormset(instance=self.object)
+        return context
+
+    def form_valid(self, form):
+        super().form_valid(form)
+        extra = TargetExtraFormset(self.request.POST, instance=self.object)
+        if extra.is_valid():
+            extra.save()
+        return redirect(self.get_success_url())
 
 
 class TargetDelete(DeleteView):
