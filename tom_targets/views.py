ba085367cdda2447d836a4ffc5c4823dd7b31269
David Collom <dmcollom@gmail.com>
2018-10-02 14:36:03 -0700

    fixed key error

--- tom_targets/views.py
+++ tom_targets/views.py
@@ -127,7 +127,7 @@ class TargetObservationPlan(SingleObjectMixin, FormView):
     def form_valid(self, form):
         start_time = form.cleaned_data['start_time']
         end_time = form.cleaned_data['end_time']
-        airmass_limit = form.cleaned_data['airmass_limit']
+        airmass_limit = form.cleaned_data['airmass']
         self.object.get_visibility(start_time, end_time, 10, airmass_limit)
         return super().form_valid(form)
 
