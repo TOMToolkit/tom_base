b0934856ddbc945d5d9946cdfc8ab81e1caedb74
David Collom <dmcollom@gmail.com>
2018-10-04 15:55:47 -0700

    modified rise_set calculation to account for rise/set intervals of less than 24 hours

--- tom_observations/utils.py
+++ tom_observations/utils.py
@@ -5,7 +5,7 @@ from tom_observations.rise_set import RiseSetTree, RiseSetPair
 
 ephem_format = '%Y/%m/%d %H:%M:%S'
 
-# TODO: modify for loop to a while loop that shifts start time to the next_setting of previous iteration
+# TODO: modify loop to not add rise/set that are out of range
 def get_rise_set(observer, target, start_time, end_time):
     observer.date = start_time
     rise_set = RiseSetTree()
@@ -15,8 +15,10 @@ def get_rise_set(observer, target, start_time, end_time):
         next_setting = datetime.strptime(str(ephem.date(observer.next_setting(target))), ephem_format)
         rise_set.add_rise_set_pair(RiseSetPair(previous_rising, next_setting))
         start_time = datetime.strptime(str(ephem.date(next_setting)), ephem_format) + timedelta(seconds=1)
-    for time in range(int(round(start_time.timestamp())), int(round(end_time.timestamp())), 60*60*24):
-        observer.date = datetime.fromtimestamp(time)
+    while start_time < end_time:
+        observer.date = start_time
         next_rising = datetime.strptime(str(ephem.date(observer.next_rising(target))), ephem_format)
         next_setting = datetime.strptime(str(ephem.date(observer.next_setting(target))), ephem_format)
+        rise_set.add_rise_set_pair(RiseSetPair(next_rising, next_setting))
+        start_time = datetime.strptime(str(ephem.date(next_setting)), ephem_format) + timedelta(seconds=1)
     return rise_set
