ef7de7caf8899ce70a227b738023c6cf429dc2a2
David Collom <dmcollom@gmail.com>
2018-12-13 14:21:31 -0800

    ensure multiple featured light curves doesn't break view

--- tom_targets/models.py
+++ tom_targets/models.py
@@ -89,7 +89,7 @@ class Target(models.Model):
         return self.dataproduct_set.get(tag='fits_file', featured=True)
 
     def light_curve(self):
-        return self.dataproduct_set.get(tag='light_curve', featured=True)
+        return self.dataproduct_set.filter(tag='light_curve', featured=True).first()
 
     def as_dict(self):
         if self.type == self.SIDEREAL:
