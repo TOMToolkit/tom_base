dde9643932fd943bb7c42def118b1831ad7021e4
Austin Riba <austin@m51.io>
2018-12-14 16:07:39 -0800

    Initial tests

--- tom_dataproducts/tests.py
+++ tom_dataproducts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse
+from unittest.mock import patch
 
-# Create your tests here.
+from tom_observations.tests.utils import FakeFacility
+from tom_observations.tests.factories import TargetFactory, ObservingRecordFactory
+from tom_dataproducts.models import DataProduct
+
+
+@patch('tom_dataproducts.models.DataProduct.get_image_data', return_value=b'image')
+class TestObservationDataViews(TestCase):
+    def setUp(self):
+        self.target = TargetFactory.create()
+        self.observation_record = ObservingRecordFactory.create(
+            target_id=self.target.id,
+            facility=FakeFacility.name,
+            parameters='{}'
+        )
+        self.data_product = DataProduct.objects.create(
+            product_id='testproductid',
+            target=self.target,
+            observation_record=self.observation_record,
+            data=SimpleUploadedFile('afile.fits', b'somedata')
+        )
+        user = User.objects.create_user(username='test', email='test@example.com')
+        self.client.force_login(user)
+
+    def test_dataproduct_list_on_target(self, dp_mock):
+        response = self.client.get(reverse('tom_targets:detail', kwargs={'pk': self.target.id}))
+        self.assertContains(response, 'afile.fits')
+
+    def test_dataproduct_list(self, dp_mock):
+        response = self.client.get(reverse('tom_dataproducts:data-list'))
+        self.assertContains(response, 'afile.fits')
