cf105ceb7e61e672fa3ebc788845e6631b805aa3
Bryan Miller <millerwbryan@gmail.com>
2018-12-26 13:54:24 -0300

    Initial observation form and triggering

--- tom_observations/facilities/gemini.py
+++ tom_observations/facilities/gemini.py
@@ -4,7 +4,7 @@ from django import forms
 from dateutil.parser import parse
 from crispy_forms.layout import Layout, Div
 from django.core.cache import cache
-
+from datetime import datetime
 
 from tom_observations.facility import GenericObservationForm
 from tom_common.exceptions import ImproperCredentialsException
@@ -15,12 +15,13 @@ try:
     GEM_SETTINGS = settings.FACILITIES['GEM']
 except AttributeError:
     GEM_SETTINGS = {
-        'portal_url': 'https://139.229.34.15:8443',
+        # 'portal_url': 'https://139.229.34.15:8443',
+        'portal_url': 'https://gsodbtest.gemini.edu:8443',
         'api_key': '',
     }
 
 PORTAL_URL = GEM_SETTINGS['portal_url']
-TERMINAL_OBSERVING_STATES = []
+TERMINAL_OBSERVING_STATES = ['TRIGGERED', 'ON_HOLD']
 SITES = {
     'Cerro Pachon': {
         'sitecode': 'cpo',
@@ -68,50 +69,87 @@ def flatten_error_dict(form, error_dict):
     return non_field_errors
 
 
-def get_instruments():
-    response = make_request(
-        'GET',
-        PORTAL_URL + '/api/instruments/',
-        headers={'Authorization': 'Token {0}'.format(GEM_SETTINGS['api_key'])}
-    )
-    return response.json()
-
-
-def instrument_choices():
-    return [(k, k) for k in get_instruments()]
-
-
-def filter_choices():
-    return set([(f, f) for ins in get_instruments().values() for f in ins['filters']])
+# def get_instruments():
+#     response = make_request(
+#         'GET',
+#         PORTAL_URL + '/api/instruments/',
+#         headers={'Authorization': 'Token {0}'.format(GEM_SETTINGS['api_key'])}
+#     )
+#     return response.json()
+# 
+# 
+# def instrument_choices():
+#     return [(k, k) for k in get_instruments()]
+# 
+# 
+# def filter_choices():
+#     return set([(f, f) for ins in get_instruments().values() for f in ins['filters']])
 
 
 def proposal_choices():
-    response = make_request(
-        'GET',
-        PORTAL_URL + '/api/profile/',
-        headers={'Authorization': 'Token {0}'.format(GEM_SETTINGS['api_key'])}
-    )
-    choices = []
-    for p in response.json()['proposals']:
-        if p['current']:
-            choices.append((p['id'], '{} ({})'.format(p['title'], p['id'])))
+    choices = ''
+#     response = make_request(
+#         'GET',
+#         PORTAL_URL + '/api/profile/',
+#         headers={'Authorization': 'Token {0}'.format(GEM_SETTINGS['api_key'])}
+#     )
+#     choices = []
+#     for p in response.json()['proposals']:
+#         if p['current']:
+#             choices.append((p['id'], '{} ({})'.format(p['title'], p['id'])))
     return choices
 
 
 class GEMObservationForm(GenericObservationForm):
-    group_id = forms.CharField()
-    proposal = forms.ChoiceField(choices=proposal_choices)
-    ipp_value = forms.FloatField()
-    start = forms.CharField(widget=forms.TextInput(attrs={'type': 'date'}))
-    end = forms.CharField(widget=forms.TextInput(attrs={'type': 'date'}))
-    filter = forms.ChoiceField(choices=filter_choices)
-    instrument_name = forms.ChoiceField(choices=instrument_choices)
-    exposure_count = forms.IntegerField(min_value=1)
-    exposure_time = forms.FloatField(min_value=0.1)
-    max_airmass = forms.FloatField()
-    observation_type = forms.ChoiceField(
-        choices=(('NORMAL', 'Normal'), ('TARGET_OF_OPPORTUNITY', 'Rapid Response'))
+    progid = forms.CharField()
+    userkey = forms.CharField()
+    email = forms.CharField()
+    obsnum = forms.IntegerField(min_value=1)
+    ready = forms.ChoiceField(
+        choices=(('false', 'No'), ('true', 'Yes'))
     )
+    brightness = forms.FloatField(required=False)
+    brightness_system =forms.ChoiceField(required=False,
+        choices=(('Vega', 'Vega'), ('AB', 'AB'), ('JY', 'Jy'))
+    )
+    brightness_band = forms.ChoiceField(required=False,
+        choices=(('u', 'u'), ('U', 'U'), ('B', 'B'), ('g', 'g'), ('V', 'V'), ('UC', 'UC'), ('r', 'r'), ('R', 'R'),
+                 ('i', 'i'), ('I', 'I'), ('z', 'z'), ('Y', 'Y'), ('J', 'J'), ('H', 'H'), ('K', 'K'), ('L', 'L'),
+                 ('M', 'M'), ('N', 'N'), ('Q', 'Q'), ('AP', 'AP'))
+    )
+    posangle = forms.FloatField(min_value=0., max_value=360., required=False)
+    # posangle = forms.FloatField(min_value=0., max_value=360.,help_text="Position angle in degrees [0-360]")
+    pamode = forms.ChoiceField(required=False,
+        choices=(('FLIP', 'Flip180'), ('FIXED', 'Fixed'), ('FIND', 'Find'), ('PARALLACTIC', 'Parallactic'))
+    )
+    group = forms.CharField(required=False)
+    note = forms.CharField(required=False)
+
+    # gstarg = forms.CharField(required=False)
+    # gsra = forms.CharField(required=False)
+    # gsdec = forms.CharField(required=False)
+    # gsbrightness = forms.FloatField(required=False)
+    # gsbrightness_system =forms.ChoiceField(required=False,
+    #     choices=(('VEGA', 'Vega'), ('AB', 'AB'), ('JY', 'Jy'))
+    # )
+    # gsbrightness_band = forms.ChoiceField(required=False,
+    #     choices=(('UP', 'u'), ('U', 'U'), ('B', 'B'), ('GP', 'g'), ('V', 'V'), ('UC', 'UC'), ('RP', 'r'), ('R', 'R'),
+    #              ('IP', 'i'), ('I', 'I'), ('ZP', 'z'), ('Y', 'Y'), ('J', 'J'), ('H', 'H'), ('K', 'K'), ('L', 'L'),
+    #              ('M', 'M'), ('N', 'N'), ('Q', 'Q'), ('AP', 'AP'))
+    # )
+    #
+    # obsdate = forms.CharField(required=False,widget=forms.TextInput(attrs={'type': 'date'}))
+
+    #     start = forms.CharField(widget=forms.TextInput(attrs={'type': 'date'}))
+#     end = forms.CharField(widget=forms.TextInput(attrs={'type': 'date'}))
+#     filter = forms.ChoiceField(choices=filter_choices)
+#     instrument_name = forms.ChoiceField(choices=instrument_choices)
+#     exposure_count = forms.IntegerField(min_value=1)
+#     exposure_time = forms.FloatField(min_value=0.1)
+#     max_airmass = forms.FloatField()
+#     observation_type = forms.ChoiceField(
+#         choices=(('NORMAL', 'Normal'), ('TARGET_OF_OPPORTUNITY', 'Rapid Response'))
+#     )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -119,11 +157,15 @@ class GEMObservationForm(GenericObservationForm):
             self.common_layout,
             Div(
                 Div(
-                    'group_id', 'proposal', 'ipp_value', 'observation_type', 'start', 'end',
+                    'progid', 'obsnum', 'brightness', 'group',
                     css_class='col'
                 ),
                 Div(
-                    'filter', 'instrument_name', 'exposure_count', 'exposure_time', 'max_airmass',
+                    'email', 'posangle', 'brightness_band', 'note',
+                    css_class='col'
+                ),
+                Div(
+                    'userkey', 'pamode', 'brightness_system', 'ready',
                     css_class='col'
                 ),
                 css_class='form-row'
@@ -145,65 +187,61 @@ class GEMObservationForm(GenericObservationForm):
             self.add_error(None, flatten_error_dict(self, errors))
         return not errors
 
-    def instrument_to_type(self, instrument_name):
-        if any(x in instrument_name for x in ['FLOYDS', 'NRES']):
-            return 'SPECTRUM'
-        else:
-            return 'EXPOSE'
+#     def instrument_to_type(self, instrument_name):
+#         if any(x in instrument_name for x in ['FLOYDS', 'NRES']):
+#             return 'SPECTRUM'
+#         else:
+#             return 'EXPOSE'
 
     @property
     def observation_payload(self):
         target = Target.objects.get(pk=self.cleaned_data['target_id'])
-        return {
-            "group_id": self.cleaned_data['group_id'],
-            "proposal": self.cleaned_data['proposal'],
-            "ipp_value": self.cleaned_data['ipp_value'],
-            "operator": "SINGLE",
-            "observation_type": self.cleaned_data['observation_type'],
-            "requests": [
-                {
-                    "target": {
-                        "name": target.name,
-                        "type": target.type,
-                        "ra": target.ra,
-                        "dec": target.dec,
-                        "proper_motion_ra": target.pm_ra,
-                        "proper_motion_dec": target.pm_dec,
-                        "epoch": target.epoch,
-                        "orbinc": target.inclination,
-                        "longascnode": target.lng_asc_node,
-                        "argofperih": target.arg_of_perihelion,
-                        "perihdist": target.distance,
-                        "meandist": target.semimajor_axis,
-                        "meananom": target.mean_anomaly,
-                        "dailymot": target.mean_daily_motion
-                    },
-                    "molecules": [
-                        {
-                            "type": self.instrument_to_type(self.cleaned_data['instrument_name']),
-                            "instrument_name": self.cleaned_data['instrument_name'],
-                            "filter": self.cleaned_data['filter'],
-                            "spectra_slit": self.cleaned_data['filter'],
-                            "exposure_count": self.cleaned_data['exposure_count'],
-                            "exposure_time": self.cleaned_data['exposure_time']
-                        }
-                    ],
-                    "windows": [
-                        {
-                            "start": self.cleaned_data['start'],
-                            "end": self.cleaned_data['end']
-                        }
-                    ],
-                    "location": {
-                        "telescope_class": self.cleaned_data['instrument_name'][:3].lower()
-                    },
-                    "constraints": {
-                        "max_airmass": self.cleaned_data['max_airmass'],
-                    }
-                }
-            ]
+
+        payload = {
+            "prog": self.cleaned_data['progid'],
+            "password": self.cleaned_data['userkey'],
+            "email": self.cleaned_data['email'],
+            "obsnum": self.cleaned_data['obsnum'],
+            "target": target.name,
+            "ra": target.ra,
+            "dec": target.dec,
+            "note": self.cleaned_data['note'],
+            "posangle": str(self.cleaned_data['posangle']).strip(),
+            "ready": self.cleaned_data['ready']
         }
 
+        if str(self.cleaned_data['brightness']).strip() != '':
+            smags = str(self.cleaned_data['brightness']).strip() + '/' + \
+                self.cleaned_data['brightness_band'].strip() + '/' + \
+                self.cleaned_data['brightness_system'].strip()
+            payload["mags"] = smags
+
+        if self.cleaned_data['group'].strip() != '':
+            payload['group'] = self.cleaned_data['group'].strip()
+
+        # # Guide star
+        # if self.cleaned_data['gstarg'].strip() != '':
+        #     payload['gstarget'] = self.cleaned_data['gstarg']
+        #     payload['gsra'] = self.cleaned_data['gsra']
+        #     payload['gsdec'] = self.cleaned_data['gsdec']
+        #     sgsmag = str(self.cleaned_data['smag']).strip() + '/UC/Vega'
+        #     payload['gsmags'] = sgsmag
+        #     payload['gsprobe'] = self.cleaned_data['gsprobe']
+        #
+        # # timing window?
+        # if self.cleaned_data['wDate'].strip() != '':
+        #     payload['windowDate'] = self.cleaned_data['wDate'].strip()
+        #     payload['windowTime'] = self.cleaned_data['wTime'].strip()
+        #     payload['windowDuration'] = str(self.cleaned_data['wDur)']).strip()
+        #
+        # # elevation/airmass
+        # s_eltype = self.cleaned_data['eltype'].strip()
+        # if s_eltype == 'airmass' or s_eltype == 'hourAngle':
+        #     payload['elevationType'] = s_eltype
+        #     payload['elevationMin'] = str(self.cleaned_data['elmin']).strip()
+        #     payload['elevationMax'] = str(self.cleaned_data['elmax']).strip()
+            
+        return payload
 
 class GEMFacility(GenericObservationFacility):
     name = 'GEM'
@@ -213,11 +251,14 @@ class GEMFacility(GenericObservationFacility):
     def submit_observation(clz, observation_payload):
         response = make_request(
             'POST',
-            PORTAL_URL + '/too/',
-            json=observation_payload
+            PORTAL_URL + '/too',
+            verify=False,
+            params=observation_payload
             # headers=clz._portal_headers()
         )
-        return [r['id'] for r in response.json()['requests']]
+        # Return just observation number
+        obsid = response.text.split('-')
+        return [obsid[-1]]
 
     @classmethod
     def validate_observation(clz, observation_payload):
@@ -228,7 +269,7 @@ class GEMFacility(GenericObservationFacility):
         #     headers=clz._portal_headers()
         # )
         # return response.json()['errors']
-        return ''
+        return {}
 
     @classmethod
     def get_observation_url(clz, observation_id):
