62baa91c7ffeca7af764a43a82d9717f9027ece5
xuiris <ixu@lco.global>
2019-06-28 17:21:51 -0700

    Initial attempt at writing Target DB to a csv file. Takes no arguments.

---
+++ tom_targets/export_targets.py
@@ -0,0 +1,26 @@
+import os
+import csv
+from django.conf import settings
+from django.utils.text import slugify
+from datetime import datetime
+from .models import Target
+
+
+# this will just export all the targets existing into a csv file in folder csvTargetFiles
+# referenced https://www.codingforentrepreneurs.com/blog/django-queryset-to-csv-files-datasets/
+def export_targets():
+    # should prob pass argument. Should prob be the queryset.
+    qs = Target.objects.all().values()
+    data_list = list(qs)
+    filename = "{}-targets.csv".format(slugify(datetime.utcnow()))
+    path = os.path.join(os.path.dirname(settings.BASE_DIR), 'csvTargetFiles')
+    if not os.path.exists(path):
+        os.mkdir(path)
+    filepath = os.path.join(path, filename)
+    target_fields = [field.name for field in Target._meta.get_fields()]
+    with open(filepath, 'w') as my_csv:
+        writer = csv.DictWriter(my_csv, fieldnames=target_fields)
+        writer.writeheader()
+        for target_data in data_list:
+            writer.writerow(target_data)
+    return
