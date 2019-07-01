3be06e1c16823d1a98b7d147fb7ad019e049f4be
xuiris <ixu@lco.global>
2019-07-01 10:03:39 -0700

    Remove print statement

--- tom_targets/views.py
+++ tom_targets/views.py
@@ -30,7 +30,6 @@ class TargetListView(PermissionListMixin, FilterView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        print(filter.qs)
         context['target_count'] = context['object_list'].count()
         return context
 
