397502b240054b48ad0f6d138aea00c1440fd5b8
Austin Riba <austin@m51.io>
2019-07-08 08:46:39 -0700

    Increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tomtoolkit',
-    version='0.3.0',
+    version='0.4.0',
     description='The TOM Toolkit and base modules',
     long_description=long_description,
     long_description_content_type='text/markdown',
