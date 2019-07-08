06189696497e69927837e006e9667c8576976d85
Ara Ye <aye@lco.global>
2019-07-08 17:18:48 +0000

    fix the bug of throwing errors when target extra's value is too large, by adding OverflowError to the error handling type list

--- tom_targets/models.py
+++ tom_targets/models.py
@@ -205,18 +205,18 @@ class TargetExtra(models.Model):
     def save(self, *args, **kwargs):
         try:
             self.float_value = float(self.value)
-        except (TypeError, ValueError):
+        except (TypeError, ValueError, OverflowError):
             self.float_value = None
         try:
             self.bool_value = bool(self.value)
-        except (TypeError, ValueError):
+        except (TypeError, ValueError, OverflowError):
             self.bool_value = None
         try:
             if isinstance(self.value, datetime):
                 self.time_value = self.value
             else:
                 self.time_value = parse(self.value)
-        except (TypeError, ValueError) as e:
+        except (TypeError, ValueError, OverflowError) as e:
             self.time_value = None
 
         super().save(*args, **kwargs)
