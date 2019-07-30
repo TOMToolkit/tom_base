cc351f4f529290e321c3c0259c257cef8a7ba8bc
Ara Ye <aye@lco.global>
2019-07-30 10:22:49 -0700

    revert test_settings.py

--- test_settings.py
+++ test_settings.py
@@ -3,9 +3,9 @@ from tom_base.settings import *  # noqa
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'tom_pg',
-       'USER': 'aye',
-       'PASSWORD': 'asdfghjkl',
+       'NAME': 'tom',
+       'USER': 'postgres',
+       'PASSWORD': 'postgres',
        'HOST': '127.0.0.1',
        'PORT': 5432
    }
