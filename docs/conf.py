99e5b3ab3471b4b0e6b8fbec749d50c4af786d85
David Collom <dmcollom@gmail.com>
2019-08-27 07:20:26 -0700

    exluding numpy/astropy from autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,7 @@ source_parsers = {
 
 source_suffix = ['.rst', '.md']
 
+autodoc_mock_imports = ['numpy', 'astropy']
 
 # -- Options for HTML output -------------------------------------------------
 
