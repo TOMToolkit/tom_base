43a5dc9de61221bf4aa46099aca381b522c07642
David Collom <dmcollom@gmail.com>
2019-09-04 13:46:54 -0700

    Attempting to mock gemini dependency

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # source_suffix = ['.rst', '.md']
 
-autodoc_mock_imports = ['rise-set']
+autodoc_mock_imports = ['rise-set', 'gsselect']
 autodoc_inherit_docstrings = False
 # autodoc_default_options = {
 #     'members':         True,
