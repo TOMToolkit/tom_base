f3bdb939eb315410d211fb9b3b6bc07fa206e21f
David Collom <dmcollom@gmail.com>
2019-09-09 09:57:42 -0700

    a little more debugging

--- tom_dataproducts/data_processor.py
+++ tom_dataproducts/data_processor.py
@@ -38,6 +38,7 @@ class DataProcessor():
 
         mimetype = mimetypes.guess_type(data_product.data.path)[0]
         print(mimetype)
+        print(mimetypes.knownfiles)
         if mimetype in FITS_MIMETYPES:
             return self._process_spectrum_from_fits(data_product)
         elif mimetype in PLAINTEXT_MIMETYPES:
