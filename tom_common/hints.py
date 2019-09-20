fb5f648deb5bc02b5f6ccf4aac66868af2858568
David Collom <dmcollom@gmail.com>
2019-09-20 11:19:15 -0700

    Using passed in kwarg rather than hardcoding

--- tom_common/hints.py
+++ tom_common/hints.py
@@ -5,4 +5,4 @@ from django.contrib.messages.storage.base import Message
 
 def add_hint(request, hint_text, level=settings.HINT_LEVEL):
     if settings.HINTS_ENABLED:
-        messages.add_message(request, settings.HINT_LEVEL, hint_text)
+        messages.add_message(request, level, hint_text)
