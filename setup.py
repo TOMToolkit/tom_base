f65cd23cc2340937bf8643828152e299c2233491
David Collom <dmcollom@gmail.com>
2019-09-28 07:30:33 -0700

    Upping version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tomtoolkit',
-    version='1.0.0',
+    version='1.1.0',
     description='The TOM Toolkit and base modules',
     long_description=long_description,
     long_description_content_type='text/markdown',
