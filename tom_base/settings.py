4e7941b323af7527d90ecfba81bf68ee1f350373
David Collom <dmcollom@gmail.com>
2019-10-22 11:48:30 -0700

    Adding docs surrounding code hooks

--- tom_base/settings.py
+++ tom_base/settings.py
@@ -220,12 +220,6 @@ HOOKS = {
     'data_product_post_upload': 'tom_dataproducts.hooks.data_product_post_upload'
 }
 
-# HOOKS = {
-#     'target_post_save': 'tom_common.hooks.target_post_save',
-#     'observation_change_state': 'tom_common.hooks.observation_change_state',
-#     'data_product_post_upload': 'tom_dataproducts.hooks.data_product_post_upload'
-# }
-
 AUTO_THUMBNAILS = False
 
 THUMBNAIL_MAX_SIZE = (0, 0)
