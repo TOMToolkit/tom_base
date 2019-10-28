5d6955ef679c7698a659eea3a11f95bae454711b
David Collom <dmcollom@gmail.com>
2019-10-28 16:50:58 -0700

    more massaging of git

--- tom_targets/views.py
+++ tom_targets/views.py
@@ -424,7 +424,6 @@ class TargetAddRemoveGroupingView(LoginRequiredMixin, View):
             messages.error(request, 'Permission denied.')
             return redirect(reverse('tom_targets:list') + '?' + query_string)
 
-
         if 'add' in request.POST:
             if request.POST.get('isSelectAll') == 'True':
                 add_all_to_grouping(filter_data, grouping_object, request)
