b7056b8716030ea99f30410f3c8d1a0a1e5c9785
David Collom <dmcollom@gmail.com>
2019-12-05 13:18:54 -0800

    Updating version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tomtoolkit',
-    version='1.2.1',
+    version='1.2.2',
     description='The TOM Toolkit and base modules',
     long_description=long_description,
     long_description_content_type='text/markdown',
