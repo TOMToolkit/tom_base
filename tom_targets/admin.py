cc2c1d3f9b39ceb84348baea2ff9bfc84506fcfb
David Collom <dmcollom@gmail.com>
2019-12-06 15:01:34 -0800

    Added TargetExtra as an inline admin model

--- tom_targets/admin.py
+++ tom_targets/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Target, TargetList
+from .models import Target, TargetList, TargetExtra
+
+
+class TargetExtraInline(admin.TabularInline):
+    model = TargetExtra
+    exclude = ('float_value', 'bool_value', 'time_value')
+    extra = 0
 
 
 class TargetAdmin(admin.ModelAdmin):
     model = Target
+    inlines = [TargetExtraInline]
 
 
 class TargetListAdmin(admin.ModelAdmin):
