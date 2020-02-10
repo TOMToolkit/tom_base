b1e8ed0c4c55d3380c16b2a8faed6912c9f68ad7
David Collom <dmcollom@gmail.com>
2020-02-10 15:45:25 -0800

    Fixed tests

--- tom_observations/tests/test_cadence.py
+++ tom_observations/tests/test_cadence.py
@@ -8,7 +8,10 @@ from tom_observations.models import ObservationGroup, ObservationRecord
 from tom_observations.cadence import RetryFailedObservationsStrategy, ResumeCadenceAfterFailureStrategy
 
 
-@patch('tom_observations.facilities.lco.LCOBaseObservationForm.proposal_choices',
+mock_filters = {'1M0-SCICAM-SINISTRO': {'type': 'IMAGE', 'class': '1m0', 'name': '1.0 meter Sinistro', 'optical_elements': {'filters': [{'name': 'Bessell-I', 'code': 'I'}]}}}
+
+@patch('tom_observations.facilities.lco.LCOBaseForm._get_instruments', return_value=mock_filters)
+@patch('tom_observations.facilities.lco.LCOBaseForm.proposal_choices',
        return_value=[('LCOSchedulerTest', 'LCOSchedulerTest')])
 @patch('tom_observations.facilities.lco.LCOFacility.submit_observation', return_value=[198132])
 @patch('tom_observations.facilities.lco.LCOFacility.validate_observation')
@@ -20,7 +23,7 @@ class TestReactiveCadencing(TestCase):
         self.group.observation_records.add(*observing_records)
         self.group.save()
 
-    def test_retry_when_failed_cadence(self, patch1, patch2, patch3):
+    def test_retry_when_failed_cadence(self, patch1, patch2, patch3, patch4):
         num_records = self.group.observation_records.count()
         observing_record = self.group.observation_records.first()
         observing_record.status = 'CANCELED'
@@ -40,7 +43,7 @@ class TestReactiveCadencing(TestCase):
 
     @patch('tom_observations.facilities.lco.LCOFacility.get_observation_status', return_value={'state': 'CANCELED',
            'scheduled_start': None, 'scheduled_end': None})
-    def test_resume_when_failed_cadence_failed_obs(self, patch1, patch2, patch3, patch4):
+    def test_resume_when_failed_cadence_failed_obs(self, patch1, patch2, patch3, patch4, patch5):
         num_records = self.group.observation_records.count()
 
         strategy = ResumeCadenceAfterFailureStrategy(self.group, 72)
@@ -54,7 +57,7 @@ class TestReactiveCadencing(TestCase):
 
     @patch('tom_observations.facilities.lco.LCOFacility.get_observation_status', return_value={'state': 'COMPLETED',
            'scheduled_start': None, 'scheduled_end': None})
-    def test_resume_when_failed_cadence_successful_obs(self, patch1, patch2, patch3, patch4):
+    def test_resume_when_failed_cadence_successful_obs(self, patch1, patch2, patch3, patch4, patch5):
         num_records = self.group.observation_records.count()
         observing_record = self.group.observation_records.order_by('-created').first()
 
