b570486bd3a660b01f1fccf68c4238226c2da8fa
David Collom <dmcollom@gmail.com>
2020-02-12 12:27:04 -0800

    Removing debugging now that reactive cadencing is working

--- tom_observations/management/commands/runcadencestrategies.py
+++ tom_observations/management/commands/runcadencestrategies.py
@@ -9,8 +9,6 @@ from tom_observations.models import ObservationGroup
 class Command(BaseCommand):
     help = 'Entry point for running cadence strategies'
 
-    # def add_arguments(self, parser):
-
     def handle(self, *args, **kwargs):
         cadenced_groups = ObservationGroup.objects.exclude(cadence_strategy='')
 
@@ -18,8 +16,7 @@ class Command(BaseCommand):
             cadence_frequency = json.loads(cg.cadence_parameters)['cadence_frequency']
             strategy = get_cadence_strategy(cg.cadence_strategy)(cg, cadence_frequency)
             new_observations = strategy.run()
-            print(new_observations)
             if not new_observations:
-                return 'Nothing for cadence to modify'
+                return 'No changes from cadence strategy.'
             else:
                 return 'Cadence update completed, {0} new observations created.'.format(len(new_observations))
