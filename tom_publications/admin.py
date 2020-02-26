104add29ea8dcb31b47f64e066360e053a196c19
David Collom <dmcollom@gmail.com>
2020-02-25 16:31:36 -0800

    removing duplicate test app

--- tom_publications/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from tom_publications.models import LatexConfiguration
-
-admin.site.register(LatexConfiguration)
-
-# Register your models here.
--- tom_publications/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TomPublicationsConfig(AppConfig):
-    name = 'tom_publications'
