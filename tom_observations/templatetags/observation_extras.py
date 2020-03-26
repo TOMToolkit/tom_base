2ff94f873c84bc19d9f1f02db03aedfc7d8e0145
David Collom <dcollom@lco.global>
2020-03-26 16:03:11 -0700

    Adding observation_plan tag for real this time

--- tom_observations/templatetags/observation_extras.py
+++ tom_observations/templatetags/observation_extras.py
@@ -41,6 +41,31 @@ def observation_type_tabs(context):
     }
 
 
+@register.inclusion_tag('tom_observations/partials/observation_plan.html')
+def observation_plan(target, facility, length=7, interval=60, airmass_limit=None):
+    """
+    Displays form and renders plot for visibility calculation. Using this templatetag to render a plot requires that
+    the context of the parent view have values for start_time, end_time, and airmass.
+    """
+
+    visibility_graph = ''
+    start_time = datetime.now()
+    end_time = start_time + timedelta(days=length)
+
+    visibility_data = get_sidereal_visibility(target, start_time, end_time, interval, airmass_limit)
+    plot_data = [
+        go.Scatter(x=data[0], y=data[1], mode='lines', name=site) for site, data in visibility_data.items()
+    ]
+    layout = go.Layout(yaxis=dict(autorange='reversed'))
+    visibility_graph = offline.plot(
+        go.Figure(data=plot_data, layout=layout), output_type='div', show_link=False
+    )
+
+    return {
+        'visibility_graph': visibility_graph
+    }
+
+
 @register.inclusion_tag('tom_observations/partials/observation_list.html')
 def observation_list(target=None):
     """
