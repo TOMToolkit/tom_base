6a8759fef6df8b48f310b4eeb3ab7256a449ec2e
David Collom <dcollom@lco.global>
2020-03-30 15:46:37 -0700

    Updated version for new release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tomtoolkit',
-    version='1.3.1',
+    version='1.4.0',
     description='The TOM Toolkit and base modules',
     long_description=long_description,
     long_description_content_type='text/markdown',
