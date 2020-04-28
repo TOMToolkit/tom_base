0b71180a09ba7099bbcbaf43fd24698f893349f6
Lindy Lindstrom <llindstrom@lco.global>
2020-04-28 19:47:43 +0000

    remove Example manual facility; add SOAR

--- tom_base/settings.py
+++ tom_base/settings.py
@@ -211,7 +211,7 @@ TOM_LATEX_PROCESSORS = {
 TOM_FACILITY_CLASSES = [
     'tom_observations.facilities.lco.LCOFacility',
     'tom_observations.facilities.gemini.GEMFacility',
-    'tom_observations.facilities.manual.ExampleManualFacility',
+    'tom_observations.facilities.soar.SOARFacility',
 ]
 
 TOM_CADENCE_STRATEGIES = [
