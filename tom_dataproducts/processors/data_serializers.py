b4053ab042bbcbc681621adafe1e0900aa9954b3
Griffin Hosseinzadeh <g.hosseinzadeh90@gmail.com>
2021-02-02 13:37:41 -0800

    use energy flux in SpectrumSerializer

--- tom_dataproducts/processors/data_serializers.py
+++ tom_dataproducts/processors/data_serializers.py
@@ -9,7 +9,7 @@ class SpectrumSerializer():
     def serialize(self, spectrum):
         """
         Serializes a Spectrum1D in order to store in a ReducedDatum object. The serialization stores only what's
-        necessary to rebuild the Spectrum1D--namely, photon_flux and wavelength, and their respective units.
+        necessary to rebuild the Spectrum1D--namely, flux and wavelength, and their respective units.
 
         :param spectrum: Spectrum1D to be serialized
         :type spectrum: specutils.Spectrum1D
@@ -18,8 +18,8 @@ class SpectrumSerializer():
         :rtype: str
         """
         serialized = {}
-        serialized['photon_flux'] = spectrum.photon_flux.value.tolist()
-        serialized['photon_flux_units'] = spectrum.photon_flux.unit.to_string()
+        serialized['flux'] = spectrum.flux.value.tolist()
+        serialized['flux_units'] = spectrum.flux.unit.to_string()
         serialized['wavelength'] = spectrum.wavelength.value.tolist()
         serialized['wavelength_units'] = spectrum.wavelength.unit.to_string()
         return json.dumps(serialized)
@@ -35,7 +35,7 @@ class SpectrumSerializer():
         :rtype: specutil.Spectrum1D
         """
         data = json.loads(spectrum)
-        flux = Quantity(value=data['photon_flux'], unit=data['photon_flux_units'])
+        flux = Quantity(value=data['flux'], unit=data['flux_units'])
         wavelength = Quantity(value=data['wavelength'], unit=data['wavelength_units'])
         spectrum = Spectrum1D(flux=flux, spectral_axis=wavelength)
         return spectrum
