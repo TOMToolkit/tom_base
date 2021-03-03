eaf233dd9578f849ffae1b951e36ff72da15c847
David Collom <dcollom@lco.global>
2021-03-02 17:37:32 -0800

    Refactored user_list to use templatetag for users lsit

--- tom_common/templatetags/user_extras.py
+++ tom_common/templatetags/user_extras.py
@@ -1,17 +1,26 @@
 from django import template
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 
 register = template.Library()
 
 
 @register.inclusion_tag('auth/partials/group_list.html', takes_context=True)
 def group_list(context):
-    print('groups')
+    """
+
+    """
     return {
         'request': context['request'],
         'groups': Group.objects.all().exclude(name='Public')
     }
 
 
-# @register.inclusion_tag('auth/partials/user_list.html', takes_context=True)
-# def 
+@register.inclusion_tag('auth/partials/user_list.html', takes_context=True)
+def user_list(context):
+    """
+
+    """
+    return {
+        'request': context['request'],
+        'users': User.objects.all()
+    }
