9186ded9516417ec6e4d02fcd7412704e2ba3620
David Collom <dcollom@lco.global>
2021-03-05 10:47:07 -0800

    Fixed spectrum migration script

--- tom_dataproducts/migrations/0010_manual_20210305_fix_spectroscopy.py
+++ tom_dataproducts/migrations/0010_manual_20210305_fix_spectroscopy.py
@@ -1,8 +1,13 @@
+from astropy.units import photon, Quantity, spectral_density
 from django.db import migrations, models
+from specutils import Spectrum1D
 
 from tom_dataproducts.processors.data_serializers import SpectrumSerializer
 
 """
+This code was written by Curtis McCully for the conversion from photon_flux to flux. The below code is an adaptation
+of this code.
+
 def photon_spectrum_to_energy_spectrum(wavelength, photon_counts):
     photon_spectrum = specutils.Spectrum1D(flux=photon_counts, spectral_axis=wavelength)
     energy_spectrum = photon_spectrum.flux * (photon_spectrum.energy / u.photon)
@@ -10,22 +15,23 @@ def photon_spectrum_to_energy_spectrum(wavelength, photon_counts):
 """
 
 def photon_spectrum_to_energy_spectrum(apps, schema_editor):
-    data_products = apps.get_model('tom_dataproducts', 'DataProduct')
+    reduced_datum = apps.get_model('tom_dataproducts', 'ReducedDatum')
     spectrum_serializer = SpectrumSerializer()
-    for row in data_products.objects.all():
-        photon_spectrum = spectrum_serializer.deserialize(row.value)
-        energy_spectrum = photon_spectrum.flux * (photon_spectrum.energy / u.photon)
-        energy_spectrum_object = specutils.Spectrum1D(
+    for row in reduced_datum.objects.filter(data_type='spectroscopy'):
+        photon_counts = Quantity(value=row.value['photon_flux'], unit=row.value['photon_flux_units'])
+        wavelength = Quantity(value=row.value['wavelength'], unit=row.value['wavelength_units'])
+        photon_spectrum = Spectrum1D(flux=photon_counts, spectral_axis=wavelength)
+        energy_spectrum = photon_spectrum.flux * (photon_spectrum.energy / photon)
+        energy_spectrum_object = Spectrum1D(
                                     spectral_axis=wavelength, 
-                                    flux=energy_spectrum.to('erg / (s cm2 AA)', u.spectral_density(wavelength)))
+                                    flux=energy_spectrum.to('erg / (s cm2 AA)', spectral_density(wavelength)))
         row.value = spectrum_serializer.serialize(energy_spectrum_object)
-        row.save()
-        
+        row.save() 
 
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('tom_dataproducts', '0009_auto_20210204_2221.py')
+        ('tom_dataproducts', '0009_auto_20210204_2221')
     ]
 
     operations = [
