90f56f6e29de64caea7977b671e6bd18f40e8749
David Collom <dcollom@lco.global>
2021-04-14 15:59:34 -0700

    Fixing a linting problem

--- tom_dataproducts/hooks.py
+++ tom_dataproducts/hooks.py
@@ -6,5 +6,6 @@ logger = logging.getLogger(__name__)
 def data_product_post_upload(dp):
     logger.info(f'Running post upload hook for DataProduct: {dp}')
 
+
 def data_product_post_save(dps):
     logger.info(f'Running post save hook for DataProduct: {dps}')
