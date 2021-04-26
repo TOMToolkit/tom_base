57919510f5fdce18df15acbe6127d1ddbdd42cc1
David Collom <dcollom@lco.global>
2021-04-26 15:14:50 -0700

    Ensuring that a 404 does not return an ImproperCredentialsException

--- tom_observations/facilities/lco.py
+++ tom_observations/facilities/lco.py
@@ -104,7 +104,7 @@ muscat_exposure_mode_help = """
 
 def make_request(*args, **kwargs):
     response = requests.request(*args, **kwargs)
-    if 401 <= response.status_code < 500:
+    if 401 <= response.status_code <= 403:
         raise ImproperCredentialsException('LCO: ' + str(response.content))
     elif 400 == response.status_code:
         raise forms.ValidationError(f'LCO: {str(response.content)}')
