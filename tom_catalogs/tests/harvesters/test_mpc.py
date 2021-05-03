debc82db73c5de14f8f06f23535691fc77f5b147
David Collom <dcollom@lco.global>
2021-05-03 08:37:04 -0700

    Fixing a monumentally dumb error in the MPC canary test

--- tom_catalogs/tests/harvesters/test_mpc.py
+++ tom_catalogs/tests/harvesters/test_mpc.py
@@ -7,6 +7,6 @@ class TestMPCHarvesterCanary(TestCase):
     @override_settings(TOM_HARVESTER_CLASSES=['tom_catalogs.harvesters.mpc.MPCHarvester'])
     def test_import_incorrect_version(self):
         """Test that MPC import fails, as the available version of astroquery contains a bug."""
-        with self.assertRaisesRegex(ImportError, 'Please consult the TOM Toolkit docs and ensure that your version of '
-                                                 'astroquery is at least 0.4.2.dev0.'):
+        with self.assertRaisesRegex(ImportError, 'Please consult the TOM Toolkit harvester API docs and '
+                                                 'ensure that your version of astroquery is at least 0.4.2.dev0.'):
             self.client.get(reverse('tom_catalogs:query'))
