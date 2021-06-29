09e2d95bf0ae4872bec2767621b824695d550af5
new2128 <81988673+new2128@users.noreply.github.com>
2021-06-29 08:15:46 -0500

    Update test_gemini.py

--- tom_observations/tests/facilities/test_gemini.py
+++ tom_observations/tests/facilities/test_gemini.py
@@ -1,20 +1,16 @@
-import unittest
-from unittest import mock
-from django.test import TestCase, override_settings, tag
-from django.utils import timezone
+from django.test import TestCase
 from unittest.mock import patch
-import requests
-from requests import Response
 import json
-from tom_alerts.alerts import get_service_class
-from tom_observations.facilities.gemini import make_request, GEMFacility
+from tom_observations.facilities.gemini import make_request
 from tom_common.exceptions import ImproperCredentialsException
-from tom_targets.models import Target
-from tom_dataproducts.models import ReducedDatum
 
-#tests make_request function of the Gemini facility, modeled after test_lco
+'''
+tests make_request function of the Gemini facility, modeled after test_lco
+'''
+
 class TestMakeRequest(TestCase):
 
+    
     @patch('tom_observations.facilities.gemini.requests.request')
     def test_make_request(self, mock_request):
         mock_response = Response()
@@ -28,3 +24,4 @@ class TestMakeRequest(TestCase):
         mock_request.return_value = mock_response
         with self.assertRaises(ImproperCredentialsException):
             make_request('GET', 'google.com', headers={'test': 'test'})
+            
