0eee6da2474f5debcc8a1df8bdf45a3bff0546f0
cjoldelo <81769811+cjoldelo@users.noreply.github.com>
2021-07-08 13:34:22 -0700

    Weed unnecessary modules

--- test_attempt.py
+++ test_attempt.py
@@ -1,35 +1,10 @@
-import unittest
-import sys
-
-import json
+from django.test import TestCase
 from unittest.mock import patch
-from unittest.mock import MagicMock
 
-from django import forms
-from django.contrib.auth.models import User, Group
-from django.contrib.messages import get_messages
-from django.core.cache import cache
-from django.test import TestCase, override_settings
-from django.urls import reverse
-
-from tom_alerts.alerts import GenericBroker, GenericQueryForm, GenericUpstreamSubmissionForm, GenericAlert
 from tom_alerts.alerts import get_service_class
-from tom_alerts.exceptions import AlertSubmissionException
-from tom_alerts.models import BrokerQuery
-from tom_observations.models import ObservationRecord
-from tom_targets.models import Target
-
-# env: GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-
-# Test alert data. Normally this would come from a remote source.
-test_alerts = [
-    {'id': 1, 'name': 'Tatooine', 'timestamp': '2019-07-01', 'ra': 32, 'dec': -20, 'mag': 8, 'score': 20},
-    {'id': 2, 'name': 'Hoth', 'timestamp': '2019-07-02', 'ra': 66, 'dec': 50, 'mag': 3, 'score': 66},
-]
 
 class TestModuleImport(TestCase):
 
-#Failure to import module
     def test_import_invalid_mod(self):
         with self.subTest('Test that an invalid import returns an import error.'):
             with patch('tom_observations.api_views.get_service_class') as mock_get_service_class:
