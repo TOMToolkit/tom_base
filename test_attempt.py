10a2f3a50adcfbeb4c0d71eb30bf886b8ea1e5f8
cjoldelo <81769811+cjoldelo@users.noreply.github.com>
2021-07-08 14:06:44 -0700

    Copy/Paste Error

--- test_attempt.py
+++ test_attempt.py
@@ -1,13 +1,13 @@
 from django.test import TestCase
 from unittest.mock import patch
 
-from tom_alerts.alerts import get_service_class
+from tom_alerts.alerts import get_service_classes
 
 class TestModuleImport(TestCase):
 
     def test_import_invalid_mod(self):
         with self.subTest('Test that an invalid import returns an import error.'):
-            with patch('tom_observations.api_views.get_service_class') as mock_get_service_class:
-                mock_get_service_class.side_effect = ImportError('Import failed. Did you provide the correct path?')
+            with patch('tom_alerts.alerts.get_service_classes') as mock_get_service_classes:
+                mock_get_service_classes.side_effect = ImportError(f'Could not import {service}. Did you provide the correct path?')
                 with self.assertRaises(ImportError):
-                    mock_get_service_class()
+                    mock_get_service_classes()
