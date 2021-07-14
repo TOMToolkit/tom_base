3771b4c0e0810692343f5ed9b77d701f8750e8b5
cjoldelo <cjoldelo@gmail.com>
2021-07-14 15:28:48 -0700

    Visibility calc mods and tests

--- tom_observations/singletarget_vis.py
+++ tom_observations/singletarget_vis.py
@@ -1,46 +1,34 @@
-import numpy as np
-import astropy.units as u
-from astropy.time import Time
-from astropy.coordinates import SkyCoord
-import pytz
-
-from astroplan import Observer, FixedTarget
-from astroplan import time_grid_from_range
-from astroplan.plots import plot_airmass
 from astroplan import AltitudeConstraint, AirmassConstraint, AtNightConstraint
-from astroplan import observability_table
-from astroplan import is_observable, is_always_observable, months_observable
+from astroplan import is_observable
+from astroplan import FixedTarget
+from astropy.coordinates import SkyCoord
+from astropy.time import Time
+import astropy.units as u
 
-"""
-Visibility calculator for a single target.  Constraints include airmass, altitude,
-AtNight, and moon separation, among others.
-"""
 
-muscat_loc = Observer(
-                    longitude = -156.2568 * u.deg,
-                    latitude = 20.7082 * u.deg,
-                    elevation = 3052 * u.m,
-                    timezone = 'US/Hawaii',
-                    name = "Haleakala Observatory"
-                    )
 def timeobj(date):
     obs_night = Time(date, format='iso', scale='utc')
     return obs_night
 
-def calculate_visibility(name, ra, dec, obs_night, max_airmass):
+def calculate_visibility(name, ra, dec, obs_night, obs_location, max_airmass=2.0):
+    """
+    Visibility calculator for a single target.  Constraints include airmass, altitude,
+    AtNight, and moon separation, among others.
+    """
     try:
         coords = SkyCoord(ra*u.deg, dec*u.deg, frame='icrs')
-        target = FixedTarget(name=name, coord=coords)
-        obs_begin = muscat_loc.twilight_evening_astronomical(obs_night, which='nearest')
-        obs_end = muscat_loc.twilight_morning_astronomical(obs_night, which='next')
+        try:
+            target = FixedTarget.from_name(name)
+        except Exception:
+            target = FixedTarget(name=name, coord=coords)
+        obs_begin = obs_location.twilight_evening_astronomical(obs_night, which='nearest')
+        obs_end = obs_location.twilight_morning_astronomical(obs_night, which='next')
         observing_range = [obs_begin, obs_end]
-        constraints = [AirmassConstraint(max_airmass), AltitudeConstraint(25*u.deg, 85*u.deg), AtNightConstraint.twilight_astronomical()]
-        ever_observable = is_observable(constraints, muscat_loc, target, time_range=observing_range)
+        constraints = [AirmassConstraint(max_airmass), AltitudeConstraint(20*u.deg, 85*u.deg), AtNightConstraint.twilight_astronomical()]
+        ever_observable = is_observable(constraints, obs_location, target, time_range=observing_range)
         if ever_observable:
             pass
         else:
             raise Exception('This object is not observable by MuSCAT on this date.')
     except ValueError:
         print('Your dates were not inputted correctly.')
-        #make an error for if observing_range is empty
-        #"For accurate visibility information, please make sure your Time object is in UTC"
--- tom_observations/tests/test_singletarget_vis.py
+++ tom_observations/tests/test_singletarget_vis.py
@@ -1,43 +1,24 @@
-import unittest
-import sys
-import json
-from unittest.mock import patch
-from unittest.mock import MagicMock
-from django import forms
-
-import numpy as np
-import astropy.units as u
-from astropy.time import Time
-from astropy.coordinates import SkyCoord
-import pytz
-
-from django.test import TestCase, override_settings
-
-from astroplan import Observer, FixedTarget
-from astroplan import time_grid_from_range
-from astroplan.plots import plot_airmass
 from astroplan import AltitudeConstraint, AirmassConstraint, AtNightConstraint
-from astroplan import observability_table
-from astroplan import is_observable, is_always_observable, months_observable
-from singletarget_vis import calculate_visibility
+from astroplan import is_observable, FixedTarget
+from astropy.coordinates import SkyCoord
+from astropy.time import Time
+import astropy.units as u
+from unittest.mock import patch
 
-muscat_loc = Observer(
-                    longitude = -156.2568 * u.deg,
-                    latitude = 20.7082 * u.deg,
-                    elevation = 3052 * u.m,
-                    timezone = 'US/Hawaii',
-                    name = "Haleakala Observatory"
-                    )
+from django.test import TestCase
+
+from tom_observations.LCO_obs_locs import *
+from singletarget_vis import calculate_visibility
 
 test_target = ['Sirius', 100.7362500*u.deg, -16.6459444*u.deg]
 date = Time("2019-12-25 00:00:00", scale='utc')
 coords = SkyCoord(test_target[1], test_target[2], frame='icrs')
 target = FixedTarget(name='Sirius', coord=coords)
-obs_begin = muscat_loc.twilight_evening_astronomical(date, which='nearest')
-obs_end = muscat_loc.twilight_morning_astronomical(date, which='next')
+obs_begin = OGG.twilight_evening_astronomical(date, which='nearest')
+obs_end = OGG.twilight_morning_astronomical(date, which='next')
 observing_range = [obs_begin, obs_end]
 constraints = [AirmassConstraint(2.0), AltitudeConstraint(25*u.deg, 85*u.deg), AtNightConstraint.twilight_astronomical()]
-ever_observable = is_observable(constraints, muscat_loc, target, time_range=observing_range)
+ever_observable = is_observable(constraints, OGG, target, time_range=observing_range)
 
 class TestVisibilityCalc(TestCase):
 
@@ -55,22 +36,15 @@ class TestVisibilityCalc(TestCase):
     def test_dates(self):
         t_obs_begin = obs_begin.to_value(format='iso')
         t_obs_end = obs_end.to_value(format='iso')
-        self.assertEqual(t_obs_begin, '2019-12-25 05:10:04.800')
-        self.assertEqual(t_obs_end, '2019-12-25 15:39:42.586')
+        self.assertEqual(t_obs_begin, '2019-12-25 05:10:04.696')
+        self.assertEqual(t_obs_end, '2019-12-25 15:39:42.359')
 
     def test_ever_obs(self):
-        self.assertEqual(ever_observable, True)
+        self.assertTrue(ever_observable)
 
     def test_not_obs(self):
         with self.subTest('Test that an invalid object returns an exception.'):
             with patch('singletarget_vis.calculate_visibility') as mock_calculate_visibility:
                 mock_calculate_visibility.side_effect = Exception('This object is not observable by MuSCAT on this date.')
-                with self.assertRaises(Exception):
-                    mock_calculate_visibility()
-
-    def test_bad_dates(self):
-        with self.subTest('Test that an invalid date returns a ValueError.'):
-            with patch('singletarget_vis.calculate_visibility') as mock_calculate_visibility:
-                mock_calculate_visibility.side_effect = ValueError('Your dates were not inputted correctly.')
-                with self.assertRaises(ValueError):
-                    mock_calculate_visibility()
+                with self.assertRaisesRegex(Exception, 'not observable'):
+                    calculate_visibility('Polaris', 37.954, 89.264, Time("2019-12-25 00:00:00", scale='utc'), OGG)
