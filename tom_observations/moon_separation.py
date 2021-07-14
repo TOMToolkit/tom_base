80e03a696e9371905f58e8d8270db307232da460
cjoldelo <cjoldelo@gmail.com>
2021-07-14 15:29:53 -0700

    Moon separation function and tests

---
+++ tom_observations/moon_separation.py
@@ -0,0 +1,63 @@
+from astroplan import FixedTarget
+from astroplan import moon_illumination as moon_illum
+from astroplan import moon_phase_angle as moon_phase_angle
+import astropy.coordinates
+import astropy.units as u
+from astropy.coordinates import SkyCoord, AltAz
+from astropy.coordinates import get_moon as get_moon
+import numpy as np
+import warnings
+
+from LCO_obs_locs import *
+
+def all_night_moon_sep(name, ra, dec, obs_night, obs_loc, sample_size=25):
+    """
+    Determines the min and max separations of the target object and the moon over a full
+    observing night at Haleakala observatory. If it registers <15 degree separation at
+    minimum, it prints a warning that the target object is too close to the moon.
+    If it registers a <15 degree separation at maximum, it rejects the observation request.
+    """
+    try:
+        coords = SkyCoord(ra*u.deg, dec*u.deg, frame='icrs')
+
+        try:
+            target = FixedTarget.from_name(name)
+        except Exception:
+            target = FixedTarget(name=name, coord=coords)
+
+        obs_begin = obs_loc.twilight_evening_astronomical(obs_night, which='nearest')
+        obs_end = obs_loc.twilight_morning_astronomical(obs_night, which='next')
+        midnight = obs_loc.midnight(obs_night, which='nearest')
+        lower_lim = (obs_begin - midnight).to(u.h)
+        upper_lim = (obs_end - midnight).to(u.h)
+
+        delta_midnight = np.linspace(lower_lim.value, upper_lim.value, sample_size)*u.hour
+        frame_observing_night = AltAz(obstime=midnight+delta_midnight,
+                          location=obs_loc.location)
+        targetaltaz_obsnight = coords.transform_to(frame_observing_night)
+        moonaltaz_obsnight = get_moon(time=midnight+delta_midnight,
+                          location=obs_loc.location).transform_to(frame_observing_night)
+
+        moon_frac = moon_illum(time=midnight+delta_midnight) *100
+        avg_moonill = np.mean(moon_frac)
+        mphase = moon_phase_angle(time=midnight+delta_midnight).to(u.deg)
+        avg_mphase = np.mean(mphase)
+        # does not go to negatives: simply moves between 0 and 180 degrees, 0 being full moon and 180 being new moon
+
+        sep_array = [y.separation(x) for x,y in zip(targetaltaz_obsnight, moonaltaz_obsnight)]
+        sep_array_deg = [x.degree for x in sep_array]
+        avg_sep = np.mean(sep_array_deg)
+
+        if max(sep_array_deg) < 15:
+            raise Exception ('Object is too close to the moon on this date.')
+        if min(sep_array_deg) <= 15 and max(sep_array_deg) >= 15:
+            warnings.warn('Warning: Object is very close to the moon on this date.')
+            print('Average separation is {0:.1f} degrees'.format(avg_sep))
+        if min(sep_array_deg) >= 15:
+            print('Average moon separation is {0:.1f} degrees'.format(avg_sep))
+            print('{0:.1f} percent of the moon is illuminated'.format(avg_moonill))
+            print('The average moon phase angle is {0:.1f}'.format(avg_mphase))
+        else:
+            print('Something really weird just happened')
+    except (ValueError, AttributeError):
+        print('Your dates were not inputted correctly.')
---
+++ tom_observations/tests/test_moon_separation.py
@@ -0,0 +1,79 @@
+from astroplan import FixedTarget
+from astroplan import moon_illumination as moon_illum
+from astroplan import moon_phase_angle as moon_phase_angle
+import astropy.coordinates
+import astropy.units as u
+from astropy.time import Time
+from astropy.coordinates import SkyCoord, AltAz
+from astropy.coordinates import get_moon as get_moon
+from unittest.mock import patch
+import numpy as np
+
+from django.test import TestCase
+
+from tom_observations.LCO_obs_locs import *
+from moon_separation import all_night_moon_sep
+
+test_target = ['M31', 10.6847083*u.deg, 41.2687500*u.deg]
+date = Time("2021-05-04 00:00:00", scale='utc')
+coords = SkyCoord(test_target[1], test_target[2], frame='icrs')
+target = FixedTarget(name='M31', coord=coords)
+obs_begin = OGG.twilight_evening_astronomical(date, which='nearest')
+obs_end = OGG.twilight_morning_astronomical(date, which='next')
+midnight = OGG.midnight(date, which='nearest')
+lower_lim = (obs_begin - midnight).to(u.h)
+upper_lim = (obs_end - midnight).to(u.h)
+
+delta_midnight = np.linspace(lower_lim.value, upper_lim.value, 25)*u.hour
+frame_observing_night = AltAz(obstime=midnight+delta_midnight,
+                  location=OGG.location)
+targetaltaz_obsnight = coords.transform_to(frame_observing_night)
+moonaltaz_obsnight = get_moon(time=midnight+delta_midnight,
+                  location=OGG.location).transform_to(frame_observing_night)
+
+moon_frac = moon_illum(time=midnight+delta_midnight) *100
+avg_moonill = np.mean(moon_frac)
+mphase = moon_phase_angle(time=midnight+delta_midnight).to(u.deg)
+avg_mphase = np.mean(mphase)
+
+sep_array = [y.separation(x) for x,y in zip(targetaltaz_obsnight, moonaltaz_obsnight)]
+sep_array_deg = [x.degree for x in sep_array]
+avg_sep = np.mean(sep_array_deg)
+
+class MoonSepCalc(TestCase):
+
+    def test_coords(self):
+        self.assertEqual(coords.ra, test_target[1])
+        self.assertEqual(coords.dec, test_target[2])
+
+    def test_target(self):
+        self.assertEqual(target.name, test_target[0])
+
+    def test_dates(self):
+        t_obs_begin = obs_begin.to_value(format='iso')
+        t_obs_end = obs_end.to_value(format='iso')
+        t_midnight = midnight.to_value(format='iso')
+        self.assertEqual(t_obs_begin, '2021-05-04 06:09:24.258')
+        self.assertEqual(t_obs_end, '2021-05-04 14:33:56.825')
+        self.assertEqual(t_midnight, '2021-05-04 10:21:40.084')
+
+    def test_good_sep(self):
+        f_avg_sep = round(avg_sep, 1)
+        f_avg_moonill = round(avg_moonill, 1)
+        f_avg_mphase = round(avg_mphase.value, 1)
+
+        with self.subTest('Test the average separation.'):
+            self.assertEqual(f_avg_sep, 73.4)
+
+        with self.subTest('Test the average moon illumination.'):
+            self.assertEqual(f_avg_moonill, 43.8)
+
+        with self.subTest('Test the average moon phase angle.'):
+            self.assertEqual(f_avg_mphase, 97.2)
+
+    def test_too_close(self):
+        with self.subTest('Test that an object too close to the moon returns an exception.'):
+            with patch('moon_separation.all_night_moon_sep') as mock_all_night_moon_sep:
+                mock_all_night_moon_sep.side_effect = Exception()
+                with self.assertRaisesRegex(Exception, 'Object is too close to the moon on this date.'):
+                    all_night_moon_sep('HD 205033', 323.2651667, -19.8063111, Time("2021-05-04 00:00:00", scale='utc'), OGG)
