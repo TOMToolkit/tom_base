e8305893f398891b88ceee030b6b808dd6ed00ae
David Collom <dcollom@lco.global>
2021-08-26 21:43:02 -0700

    Fixing linting errors

--- tom_common/models.py
+++ tom_common/models.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
