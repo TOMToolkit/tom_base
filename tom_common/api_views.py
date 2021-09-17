f91f180ea44394eef0aa54a84eca66269f33d6ad
David Collom <dcollom@lco.global>
2021-09-16 17:16:05 -0700

    Restricted endpoints to just list

--- tom_common/api_views.py
+++ tom_common/api_views.py
@@ -1,18 +1,12 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import ListModelMixin
+from rest_framework.viewsets import GenericViewSet
 
 from tom_common.serializers import GroupSerializer
 
 
-# Though DRF supports using django-guardian as a permission backend without explicitly using PermissionListMixin, we
-# chose to use it because it removes the requirement that a user be granted both object- and model-level permissions,
-# and a user that has object-level permissions is understood to also have model-level permissions.
-# For whatever reason, get_queryset has to be explicitly defined, and can't be set as a property, else the API won't
-# respect permissions.
-#
-# At present, create is not restricted at all. This seems to be a limitation of django-guardian and should be revisited.
-class GroupViewSet(ModelViewSet):
+class GroupViewSet(ListModelMixin, GenericViewSet):
     """
-    Viewset for Target objects. By default supports CRUD operations.
+    Viewset for Group objects. By default supports list operation only.
     See the docs on viewsets: https://www.django-rest-framework.org/api-guide/viewsets/
     """
     serializer_class = GroupSerializer
