fe1845f2bc1cf3d35c922583e0d0477a3be0a765
Nic Wolf <nic.wolf@noirlab.edu>
2022-02-16 11:05:15 -0700

    ENH: Brokers can add an annotation to describe the use of the `GenericAlert.score` field.

--- tom_alerts/views.py
+++ tom_alerts/views.py
@@ -199,6 +199,7 @@ class RunQueryView(TemplateView):
         query = get_object_or_404(BrokerQuery, pk=self.kwargs['pk'])
         broker_class = get_service_class(query.broker)()
         alerts = broker_class.fetch_alerts(deepcopy(query.parameters))  # TODO: Should the deepcopy be in the brokers?
+        context['score_description'] = broker_class.score_description
         context['alerts'] = []
         query.last_run = timezone.now()
         query.save()
