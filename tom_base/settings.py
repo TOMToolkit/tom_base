d5c6a5651fd80029a27b8e8d7fc5575c79c5201a
Joseph Chatelain <jchatelain@lco.global>
2022-09-28 15:24:34 -0700

    add env token search to settings

--- tom_base/settings.py
+++ tom_base/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'testkey')
+LASAIR_TOKEN = os.getenv('LASAIR_TOKEN', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
