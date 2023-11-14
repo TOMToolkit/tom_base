d6709c896307604496e484a1d026d5b31f718101
William (Lindy) Lindstrom <llindstrom@lco.global>
2023-11-14 05:58:59 +0000

    implement query_service using panstarrs_api.mast_query
    this is also where we translate the form date into the MAST
    query paramaters and handle the response.content. Since this
    is a syncronous call, we don't need a dramatique task in tasks.py

--- tom_dataproducts/forced_photometry/panstarrs_service/panstarrs.py
+++ tom_dataproducts/forced_photometry/panstarrs_service/panstarrs.py
@@ -1,12 +1,23 @@
 from datetime import timedelta, datetime
+import logging
+
 from astropy.time import Time
 from crispy_forms.layout import Div, HTML
 from django import forms
 from django.conf import settings
+from django.core.files.base import ContentFile
+from django.utils import timezone
 
-import tom_dataproducts.forced_photometry.forced_photometry_service as fps
-from tom_dataproducts.tasks import atlas_query
 from tom_targets.models import Target
+import tom_dataproducts.forced_photometry.forced_photometry_service as fps
+from tom_dataproducts.models import DataProduct
+from tom_dataproducts.exceptions import InvalidFileFormatException
+from tom_dataproducts.data_processor import run_data_processor
+
+from .panstarrs_api import get_data_release_choices, get_catalog_choices, mast_query
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class PanstarrsForcedPhotometryQueryForm(fps.BaseForcedPhotometryQueryForm):
@@ -97,6 +108,10 @@ class PanstarrsForcedPhotometryQueryForm(fps.BaseForcedPhotometryQueryForm):
         )
 
     def clean(self):
+        """After cleaning the form data field-by-field, do any necessary cross-field validation.
+
+        TODO: describe where in the validatin process this method is called.
+        """
         cleaned_data = super().clean()
         if not (cleaned_data.get('min_date') or cleaned_data.get('min_date_mjd')):
             raise forms.ValidationError("Must supply a minimum date in either datetime or mjd format")
@@ -124,41 +139,91 @@ class PanstarrsForcedPhotometryService(fps.BaseForcedPhotometryService):
         """
         This method takes in the serialized data from the query form and actually
         submits the query to the service
+
+        Called from views.py ForcedPhotometryQueryView.post() if form.is_valid()
         """
-        print(f"Querying Atlas service with params: {query_parameters}")
+        logger.debug(f"Querying PanSTARRS service with params: {query_parameters}")
+
+        target = Target.objects.get(pk=query_parameters.get('target_id'))
+
+        # first, convert datetime fields to mjd if necessary
         min_date_mjd = query_parameters.get('min_date_mjd')
         if not min_date_mjd:
             min_date_mjd = Time(query_parameters.get('min_date')).mjd
         max_date_mjd = query_parameters.get('max_date_mjd')
         if not max_date_mjd and query_parameters.get('max_date'):
             max_date_mjd = Time(query_parameters.get('max_date')).mjd
+
+        # make sure target exists
         if not Target.objects.filter(pk=query_parameters.get('target_id')).exists():
             raise fps.ForcedPhotometryServiceException(f"Target {query_parameters.get('target_id')} does not exist")
 
-        if 'ATLAS' not in settings.FORCED_PHOTOMETRY_SERVICES:
-            raise fps.ForcedPhotometryServiceException("Must specify 'ATLAS' settings in FORCED_PHOTOMETRY_SERVICES")
-        if not settings.FORCED_PHOTOMETRY_SERVICES.get('ATLAS', {}).get('url'):
+        # make sure PANSTARRS service is configured
+        if 'PANSTARRS' not in settings.FORCED_PHOTOMETRY_SERVICES:
             raise fps.ForcedPhotometryServiceException(
-                "Must specify a 'url' under ATLAS settings in FORCED_PHOTOMETRY_SERVICES"
+                "Must specify 'PANSTARRS' configuration in settings.py FORCED_PHOTOMETRY_SERVICES"
             )
-        if not settings.FORCED_PHOTOMETRY_SERVICES.get('ATLAS', {}).get('api_key'):
+        if not settings.FORCED_PHOTOMETRY_SERVICES.get('PANSTARRS', {}).get('url'):
             raise fps.ForcedPhotometryServiceException(
-                "Must specify an 'api_key' under ATLAS settings in FORCED_PHOTOMETRY_SERVICES"
+                "Must specify a 'url' under PANSTARRS settings in FORCED_PHOTOMETRY_SERVICES"
             )
+        # it's not clear if this is stricly necessary, so just warn for now
+        if not settings.FORCED_PHOTOMETRY_SERVICES.get('PANSTARRS', {}).get('api_key'):
+            logger.warning('PANSTARRS api_key not specified in setings.py FORCED_PHOTOMETRY_SERVICES')
+            # raise fps.ForcedPhotometryServiceException(
+            #     "Must specify an 'api_key' under PANSTARRS settings in FORCED_PHOTOMETRY_SERVICES"
+            # )
 
-        if 'django_dramatiq' in settings.INSTALLED_APPS:
-            atlas_query.send(min_date_mjd, max_date_mjd,
-                             query_parameters.get('target_id'),
-                             self.get_data_product_type())
+        # submit the query, create the data product, and run the data product processor
+        # synchronous query, so we can return the result immediately, no dramatiq needed
+
+        # initialize the request data with target coordinates and
+        # min_ and max_date_mjd  epochMean min and max constraints
+        request_data = {
+            'ra': target.ra,
+            'dec': target.dec,
+            'radius': 0.02,  # arcsec cone search radius
+            'epochMean.min': min_date_mjd,
+            'epochMean.max': max_date_mjd,
+            'nDetections.min': query_parameters.get('min_detections'),
+        }
+
+        catalog = query_parameters.get('catalog')
+        data_release = query_parameters.get('data_release')
+        response = mast_query(table=catalog, release=data_release, request_data=request_data)
+
+        logger.debug(f'PanSTARRS query response.text: {response.text}')
+
+        # TODO: consider consistent ContentFile naming scheme between services
+        # and implementing nameing function in base class fps.BaseForcedPhotometryService
+        min_time = Time(min_date_mjd, format='mjd').datetime.strftime('%Y_%m_%d')
+        dp_name = f"panstarrs-{data_release}-{catalog}_{min_time}"
+        if max_date_mjd:
+            max_time = Time(max_date_mjd, format='mjd').datetime.strftime('%Y_%m_%d')
+            dp_name += f"-{max_time}"
+        dp_name += '.csv'
+
+        file = ContentFile(response.content, name=dp_name)
+        # TODO: check content for 0-bytes
+
+        # TODO: this should be a get_or_create()
+        dp, created = DataProduct.objects.get_or_create(
+            product_id=dp_name,
+            target=target,
+            data=file,
+            data_product_type=self.get_data_product_type(),
+            extra_data=f'Queried from PanSTARRS (via MAST) within the TOM on {timezone.now().isoformat()}'
+        )
+        if created:
+            logger.info(f"Created dataproduct {dp_name} from PanSTARRS (MAST) query")
         else:
-            query_succeeded = atlas_query(min_date_mjd, max_date_mjd,
-                                          query_parameters.get('target_id'),
-                                          self.get_data_product_type())
-            if not query_succeeded:
-                raise fps.ForcedPhotometryServiceException(
-                    "Atlas query failed, check the server logs for more information"
-                )
-            self.success_message = "Atlas query completed. View its data product in the 'Manage Data' tab"
+            logger.warning(f"DataProduct {dp_name} already exists, skipping creation")
+
+        try:
+            run_data_processor(dp)
+        except InvalidFileFormatException as e:
+            logger.error(f"Error processing returned PanSTARRS data into ReducedDatums: {repr(e)}")
+            return False
 
         return True
 
@@ -169,7 +234,7 @@ class PanstarrsForcedPhotometryService(fps.BaseForcedPhotometryService):
 
         Typically called by the .is_valid() method.
         """
-        print(f"Validating Atlas service with params: {query_parameters}")
+        logger.info(f"Validating PanSTARRS service with params: {query_parameters}")
 
     def get_success_message(self):
         return self.success_message
