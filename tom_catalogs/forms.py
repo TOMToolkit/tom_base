5866baa6e689964eac138955b2a447da3afffb33
Joseph Chatelain <jchatelain@lco.global>
2023-12-12 12:48:28 -0800

    Fix TNS Catalog search and add help text

--- tom_catalogs/forms.py
+++ tom_catalogs/forms.py
@@ -7,7 +7,14 @@ class CatalogQueryForm(forms.Form):
     Form used for catalog harvesters ``CatalogQueryView``.
     """
     term = forms.CharField()
-    service = forms.ChoiceField(choices=lambda: [(key, key) for key in get_service_classes().keys()])
+    catalog_choices = []
+    # Build a list of catalog choices for the form including Help text if available
+    for catalog_name in get_service_classes().keys():
+        if getattr(get_service_classes()[catalog_name], "help_text", None):
+            catalog_choices.append((catalog_name, f'{catalog_name} -- {get_service_classes()[catalog_name].help_text}'))
+        else:
+            catalog_choices.append((catalog_name, catalog_name))
+    service = forms.ChoiceField(choices=catalog_choices)
 
     def get_target(self):
         """
