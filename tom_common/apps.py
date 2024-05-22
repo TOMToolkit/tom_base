f1974f31564dc9fcf99cd6021692e032688f5b58
William (Lindy) Lindstrom <llindstrom@lco.global>
2024-05-22 22:54:05 +0100

    create TOMToolkitAppConfig base class
    All TOM Toolkit INSTALLED_APPs should inherit from this class
    rather than the standard django.apps.AppConfig class that the
    startapp management command uses by default. TOMToolkitAppConfig
    defines (will define)  the instrospectable API of a TOM Toolkit app
    and the methods tom_base will call for app-specific customizations.

--- tom_common/apps.py
+++ tom_common/apps.py
@@ -1,12 +1,32 @@
+import logging
+
 from django.apps import AppConfig
 from django.conf import settings
+
 import plotly.io as pio
 
 
-class TomCommonConfig(AppConfig):
+logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+
+
+class TOMToolkitAppConfig(AppConfig):
+    """Base class for TOM Toolkit apps.
+
+    This class defines a common API for TOM Toolkit apps to use to so tom_base
+    knows how to interact with them (i.e. where are the integration points, etc.)
+    """
+
+    def ready(self):
+        super().ready()
+        logger.debug(f'{self.name} is a TOM Toolkit app and is ready')
+
+
+class TomCommonConfig(TOMToolkitAppConfig):
     name = 'tom_common'
 
     def ready(self):
+        super().ready()
         # Set default plotly theme on startup
         valid_themes = ['plotly', 'plotly_white', 'plotly_dark', 'ggplot2', 'seaborn', 'simple_white', 'none']
 
