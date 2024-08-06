97de52341a3713db971d49d1779944ea1691b130
sglasberg <sglasberg1@gmail.com>
2024-08-06 10:23:12 -0700

    wrote an extra test

--- tom_targets/merge.py
+++ tom_targets/merge.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-
+from tom_targets.models import TargetName
 
 def merge_error_message(request):
     messages.warning(request, "Please select two targets to merge!")
@@ -7,23 +7,16 @@ def merge_error_message(request):
 def target_merge(primary_target, secondary_target):
     """
     """
-    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    # for each in primary_target._meta.fields:
-    #     print(each)
-
+   
     model_fields = primary_target._meta.fields
     for field in model_fields:
-        print(f"the field is {field}")
         if  getattr(primary_target, field.name, None) is None and getattr(secondary_target, field.name, None) is not None:
             setattr(primary_target, field.name, getattr(secondary_target, field.name, None))
             primary_target.save()
-    different_fields = filter(lambda field: getattr(primary_target, field.name, None) != getattr(secondary_target, field.name, None), model_fields)
-    # new_fields = filter(lambda field: getattr(primary_target,field.name, None) == getattr(secondary_target,field.name, None), model_fields)
-    print(list(different_fields))
-    print("hi hi hi")
-    print(primary_target)
-    print("hello")
-    print("i did it!!!!")
-    # print(list(new_fields))
+
+    new_name = TargetName(target=primary_target, name=secondary_target.name)
+    new_name.save()
+
+    secondary_target.delete()
 
     return primary_target
