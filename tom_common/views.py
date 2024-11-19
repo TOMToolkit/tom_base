3d2253945aa57abfbe864b1145bf7276eb9e9738
Joey Chatelain <jchate6@gmail.com>
2024-11-18 16:20:59 -0800

    prevent non-superusers from going to other user profile page

--- tom_common/views.py
+++ tom_common/views.py
@@ -89,6 +89,16 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     template_name = 'tom_common/user_profile.html'
     model = User
 
+    def dispatch(self, *args, **kwargs):
+        """
+        Directs the class-based view to the correct method for the HTTP request method. Ensures that non-superusers
+        are not incorrectly updating the profiles of other users.
+        """
+        if not self.request.user.is_superuser and self.request.user.id != self.kwargs['pk']:
+            return redirect('user-profile', self.request.user.id)
+        else:
+            return super().dispatch(*args, **kwargs)
+
 
 class UserPasswordChangeView(SuperuserRequiredMixin, FormView):
     """
