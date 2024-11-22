affc746d398b95716b58f99bbae97996afcba1c6
Joey Chatelain <jchate6@gmail.com>
2024-11-22 14:10:25 -0800

    add profile model

--- tom_common/models.py
+++ tom_common/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db.models.signals import post_save
+from django.db import models
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
 
@@ -8,3 +10,9 @@ from rest_framework.authtoken.models import Token
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+class Profile(models.Model):
+    """Profile model for a TOMToolkit User"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    affiliation = models.CharField(max_length=100, null=True, blank=True)
