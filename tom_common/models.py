a8d61423a4f99aa770cc5afb9c8f55167064c9c7
Joey Chatelain <jchate6@gmail.com>
2024-11-23 12:41:14 -0800

    add signals to update and create profiles when users are saved.

--- tom_common/models.py
+++ tom_common/models.py
@@ -16,3 +16,6 @@ class Profile(models.Model):
     """Profile model for a TOMToolkit User"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     affiliation = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
---
+++ tom_common/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save, pre_delete
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from tom_common.models import Profile
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    """When a new user is created, create a profile for them."""
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    """When a user is saved, save their profile."""
+    instance.profile.save()
