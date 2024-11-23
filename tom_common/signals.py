5f7cf90b940ac2145ea044c8ad39757a25cdc717
Joey Chatelain <jchate6@gmail.com>
2024-11-23 12:49:23 -0800

    fix naming issue

--- tom_common/signals.py
+++ tom_common/signals.py
@@ -5,7 +5,7 @@ from tom_common.models import Profile
 
 
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, created, **kwargs):
+def create_profile(sender, instance, created, **kwargs):
     """When a new user is created, create a profile for them."""
     if created:
         Profile.objects.create(user=instance)
