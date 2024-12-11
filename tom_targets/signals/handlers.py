fa299f0b6d591c94762cb1ee13da7da4e7967bf9
Jon <jnation@lco.global>
2024-12-11 20:30:39 -0100

    Rename persistent sharing to continuously share in templates

--- tom_targets/signals/handlers.py
+++ tom_targets/signals/handlers.py
@@ -6,8 +6,8 @@ from tom_dataproducts.sharing import continuous_share_data
 
 
 @receiver(post_save, sender=ReducedDatum)
-def cb_dataproduct_post_save(sender, instance, *args, **kwargs):
-    # When a new dataproduct is created or updated, check for any persistentshare instances on that target
+def cb_reduceddatum_post_save(sender, instance, *args, **kwargs):
+    # When a new ReducedDatum is created or updated, check for any persistentshare instances on that target
     # and if they exist, attempt to share the new data
     target = instance.target
     continuous_share_data(target, reduced_datums=[instance])
