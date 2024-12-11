1354c7691e1ae5447dbcb13dd0c05439bf47728c
Joey Chatelain <jchate6@gmail.com>
2024-12-10 16:13:34 -0800

    remove redundant signal

--- tom_common/signals.py
+++ tom_common/signals.py
@@ -5,13 +5,6 @@ from tom_common.models import Profile
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    """When a new user is created, create a profile for them."""
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     """When a user is saved, save their profile."""
     # Take advantage of the fact that logging in updates a user's last_login field
