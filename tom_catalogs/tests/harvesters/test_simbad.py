74f15b7afdacd0c486a69db5f6c46be7a6c8656b
Joey Chatelain <jchate6@gmail.com>
2025-01-24 16:25:58 -0800

    fix simbad harvester to account for updates to astroquery

--- tom_catalogs/tests/harvesters/test_simbad.py
+++ tom_catalogs/tests/harvesters/test_simbad.py
@@ -10,9 +10,9 @@ from tom_catalogs.harvesters.simbad import SimbadHarvester
 class TestSimbadHarvester(TestCase):
     def setUp(self):
         self.broker = SimbadHarvester()
-        table_data = {'RA_d': [10.68470800], 'DEC_d': [41.26875000],
-                      'PMRA': ['--'], 'PMDEC': ['--'], 'ID': ['M 31, 2C 56, DA 21'],
-                      'Distance_distance': [0.8200]}
+        table_data = {'ra': [10.68470800], 'dec': [41.26875000],
+                      'pmra': ['--'], 'pmdec': ['--'], 'main_id': ['M 31, 2C 56, DA 21'],
+                      'mesdistance.dist': [0.8200], 'mesdistance.unit': ['Mpc']}
         self.broker.catalog_data = Table(table_data)
 
     def test_query_failure(self):
@@ -23,11 +23,11 @@ class TestSimbadHarvester(TestCase):
 
     def test_to_target(self):
         target = self.broker.to_target()
-        self.assertEqual(target.ra, self.broker.catalog_data['RA_d'])
-        self.assertEqual(target.dec, self.broker.catalog_data['DEC_d'])
+        self.assertEqual(target.ra, self.broker.catalog_data['ra'])
+        self.assertEqual(target.dec, self.broker.catalog_data['dec'])
         self.assertEqual(target.pm_ra, None)
         self.assertEqual(target.pm_dec, None)
-        self.assertEqual(target.distance, self.broker.catalog_data['Distance_distance'])
+        self.assertEqual(target.distance, self.broker.catalog_data['mesdistance.dist'] *1000000)
         self.assertEqual(target.name, 'M31')
 
 
