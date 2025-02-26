88f7c173771f6be08dd5d51df783faa7e7e6eabf
Austin Riba <austin@m51.io>
2025-02-25 20:35:31 -0800

    Docstring for target permissions module

--- tom_targets/permissions.py
+++ tom_targets/permissions.py
@@ -4,6 +4,23 @@ from tom_targets.models import Target
 
 
 def targets_for_user(user, qs, action):
+    """
+    This is a wrapper function for django-guardian's get_objects_for_user function
+    that attempts to mitigate performance issues with TOMs that have large targets
+    that are not private. It works by splitting the queryset into private and public
+    targets and then checking permissions only for the private targets.
+
+    :param user: The user for whom to retrieve targets.
+    :type user: User
+
+    :param qs: The queryset of targets to filter.
+    :type qs: QuerySet
+
+    :param action: The action to check permissions for.
+    :type action: str
+
+    :returns: The filtered queryset of targets.
+    """
     assert action in ['view_target', 'change_target', 'delete_target']
 
     if user.is_authenticated:
@@ -17,4 +34,4 @@ def targets_for_user(user, qs, action):
             return public_targets | get_objects_for_user(user, f'{Target._meta.app_label}.{action}', private_targets)
     else:
         # Only allow open targets
-        return qs.exclude(permissions__in=[Target.Permissions.PUBLIC, Target.Permissions.PRIVATE])
+        return qs.filter(permissions=Target.Permissions.OPEN)
