d7eaa533f8e0b2046b83471f7bf909693c3ab343
William (Lindy) Lindstrom <llindstrom@lco.global>
2025-03-18 18:11:41 +0000

    move cipher and associate signals from models.py to signals.py
    We'll store the cipher in the Session and not as a runtime
    property of the Profile, because the cipher must persist
    across requests. Because there are several signal receivers
    associate with the encryption of sensitive data, we'll just
    put the cipher create code in signals.py as well.

--- tom_common/models.py
+++ tom_common/models.py
@@ -1,92 +1,11 @@
-import base64
-
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.backends import default_backend
-
-from django.conf import settings
-from django.db.models.signals import post_save
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.signals import user_logged_in, user_logged_out
-
-from rest_framework.authtoken.models import Token
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
-
-
-# Signal: Set cipher on login
-@receiver(user_logged_in)
-def set_cipher_on_login(sender, request, user, **kwargs):
-    """When the user logs in, capture their password and use it to
-    generate and cache a Fernet instance for user-specific encryption."""
-    if hasattr(user, "profile"):
-        password = request.POST.get("password")  # Capture password from login
-        if password:
-            user.profile.create_cipher(password)  # Generates and caches the Fernet instance
-
-
-# Signal: Clear cipher on logout
-@receiver(user_logged_out)
-def clear_cipher_on_logout(sender, request, user, **kwargs):
-    """Clear the cipher when a user logs out."""
-    if hasattr(user, "profile"):
-        user.profile.clear_cipher()
 
 
 class Profile(models.Model):
     """Profile model for a TOMToolkit User"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     affiliation = models.CharField(max_length=100, null=True, blank=True)
-
-    # Here, we're setting up a mechanism to encrypt sensitive User data like
-    # API keys and password for Facilities and data services, etc. The Signals
-    # above create and clear the _cipher upon login and logout, respectively.
-    # We use the user_logged_in signal to intercept the User's password and
-    # use that to derive the encryption_key and use that to create the cipher.
-    _cipher: Fernet = None
-
-    @property
-    def cipher(self) -> Fernet:
-        """Return the User's Fernet cipher"""
-        if self._cipher is None:
-            # TODO: if this should occur, lazily instantiate - must prompt user for password
-            raise ValueError("Cipher not created. Please create it with a password.")
-        return self._cipher
-
-    def create_cipher(self, password: str):
-        """Create a Fernet cipher and save it to be used to encrypt API keys and
-        other external service credentials for this User. Uses their login password
-        to generate the encryption_key.
-
-        see https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet
-        """
-
-        # Generate a salt from hash and username
-        salt = hashes.Hash(hashes.SHA256(), backend=default_backend())
-        salt.update(self.user.username.encode())
-
-        # Derive a key using PBKDF2-HMAC and the newly generated salt
-        key_derivation_function = PBKDF2HMAC(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt=salt.finalize()[:16],  # can only finalize once; returns bytes; use 16 bytes
-            iterations=1_000_000,  # Django recommendation of jan-2025
-            backend=default_backend()
-        )
-        encryption_key = base64.urlsafe_b64encode(key_derivation_function.derive(password.encode()))
-
-        # create and cache cipher
-        self._cipher = Fernet(encryption_key)
-
-    def clear_cipher(self):
-        """Clear the cached Fernet instance (e.g., on logout)."""
-        self._cipher = None
 
     def __str__(self):
         return f'{self.user.username} Profile'
--- tom_common/signals.py
+++ tom_common/signals.py
@@ -1,9 +1,16 @@
+import base64
 import logging
 
-from django.conf import settings
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 
-from django.db.models.signals import post_save
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from rest_framework.authtoken.models import Token
@@ -11,16 +18,14 @@ from rest_framework.authtoken.models import Token
 from tom_common.models import Profile
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# NOTE: there are two ways to reference the User model,
+# see https://docs.djangoproject.com/en/5.2/topics/auth/customizing/#referencing-the-user-model
+# Basically, settings.AUTH_USER_MODEL is for code that is executed upon import,
+# while get_user_model() is valid after INSTALLED_APPS are loaded.
 
 
-# Signal: create DRF token for new users
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
-
-
-# Signal: create Profile for new users
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     """When a user is saved, save their profile."""
@@ -31,3 +36,133 @@ def save_profile(sender, instance, **kwargs):
     except User.profile.RelatedObjectDoesNotExist:
         logger.info(f'No Profile found for {instance}. Creating Profile.')
         Profile.objects.create(user=instance)
+
+
+# Signal: Create a token for the User when the User instance is created
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """Create a token for the User when the User instance is created.
+
+    For more information, see the Django REST framework documentation:
+    https://www.django-rest-framework.org/api-guide/authentication/#tokenauthentication
+    """
+    if created:
+        Token.objects.create(user=instance)
+
+
+# Here, we're setting up a mechanism to encrypt sensitive User data like
+# API keys and password for Facilities and data services, etc. The Signals
+# above create and clear the pher upon login and logout, respevely.
+# We use the user_logged_in signal to intercept the User's password and
+# use that to derive the encryption_key and use that to create the cipher.
+
+def get_session_cipher(user) -> Fernet:
+    """Return the User's Fernet cipher"""
+    cipher = user.request.session.get('cipher')
+    if cipher:
+        return Fernet(cipher)
+    return None
+
+
+def create_cipher(user, password: str) -> Fernet:
+    """Create a Fernet cipher and save it to be used to encrypt API keys and
+    other external service credentials for this User. Uses their login password
+    to generate the encryption_key.
+
+    see https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet
+    """
+
+    # Generate a salt from hash and username
+    salt = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    salt.update(user.username.encode())
+
+    # Derive encryption_key using PBKDF2-HMAC and the newly generated salt
+    kdf = PBKDF2HMAC(  # key derivation function
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt.finalize()[:16],  # only finalize once; returns bytes; use 16 bytes
+        iterations=1_000_000,  # Django recommendation of jan-2025
+        backend=default_backend(),
+    )
+    encryption_key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+
+    cipher = Fernet(encryption_key)  # create cipher
+    return cipher
+
+
+# Signal: Set cipher on login
+@receiver(user_logged_in)
+def set_cipher_on_login(sender, request, user, **kwargs) -> None:
+    """When the user logs in, capture their password and use it to
+    generate a cipher (Fernet instance). Save it in the User's Session.
+
+    The cipher will be used to encrypt/decrypt sensitive user data (passwords
+    and API keys).
+    """
+    logger.debug(f"User {user.username} has logged in. sender: {sender}; request: {request}")
+
+    if hasattr(user, "profile"):
+        password = request.POST.get("password")  # Capture password from login
+        if password:
+            logger.debug(f"Creating cipher for user: {user.username} with password {password}")
+            cipher = create_cipher(user, password)
+            request.session['cipher'] = cipher  # save cipher in session
+
+
+# Signal: Clear cipher on logout
+@receiver(user_logged_out)
+def clear_cipher_on_logout(sender, request, user, **kwargs) -> None:
+    """Clear the cipher when a user logs out."""
+    logger.debug(f"User {user.username} has logged out. sender: {sender}; request: {request}")
+    # examine cipher before clearing it
+    cipher = request.session.get('cipher')
+    if cipher:
+        logger.debug(f'Cipher exists ({cipher}), proceeding to clear it.')
+    request.session.pop('cipher', None)
+
+
+def reencrypt_sensitive_data(password):
+    """Re-encrypt the user's sensitive data with the new password."""
+    logger.debug("Re-encrypting sensitive data...")
+    logger.debug("re-create cipher")
+    # TODO: create mechanism for know what the sensitive data Fields are across all INSTALLTED_APPs and their models
+    logger.debug("use new cipher to re-encrypt data")
+    logger.debug("save newly re-encrypted data")
+    logger.debug("save save new cipher in Session")
+
+
+# Signal: Update the User's sensitive data when the password changes
+@receiver(pre_save, sender=get_user_model())
+def user_updated(sender, instance, **kwargs):
+    """When the User model is saved, detect if the password has changed.
+
+    Current list of actions to be taken upon User password change:
+     * re-encrypt the user's sensitive data with the new password
+     *
+    """
+    # TODO: sort out what the signiture of the receiver should be
+    user = kwargs.get("instance", None)
+
+    if user:
+        # user.password vs. user._password:
+        # the user.password field is used for authentication (via comparison to the (hashed) password
+        # being tested for validity). The _password field is the raw password and is what we need to
+        # create a new cipher for the User's sensitive data.
+
+        # This Signal is called for ANY change to the User model, not just password changes.
+        # So, determine if the password has changed by comparing new and old (hashed) passwords.
+        new_hashed_password = user.password  # from the not-yet-saved User instance
+        try:
+            old_hashed_password = User.objects.get(id=user.id).password  # from the previously-saved User instance
+        except User.DoesNotExist:
+            old_hashed_password = None
+
+        if new_hashed_password != old_hashed_password:
+            # New password detected
+            new_raw_password = user._password  # CAUTION: this is implemenation dependent (using _<property>)
+            logger.debug(f'User {user.username} is changing their password to {new_raw_password}')
+            reencrypt_sensitive_data(new_raw_password)  # need new RAW password to re-create cipher and re-encrypt
+        else:
+            # No new password detected
+            logger.debug(f'User {user.username} is updating their profile without a password change.')
+            pass
