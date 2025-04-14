c8ca5971290880b238c0bbf979344ea9110051b9
Joey Chatelain <jchate6@gmail.com>
2025-04-14 16:10:07 -0700

    add tests

--- tom_dataservices/tests/tests.py
+++ tom_dataservices/tests/tests.py
@@ -1,18 +1,19 @@
+from abc import ABC
+
 from django import forms
 from django.test import TestCase
 
 from tom_dataservices.dataservices import BaseDataService, MissingDataException
 from tom_dataservices.forms import BaseQueryForm
+from tom_targets.models import Target
 
 
 test_query_results = {'ra': 24, 'dec': 77, 'name': 'faketarget', 'type': 'SIDEREAL'}
 
 
 class TestDataServiceForm(BaseQueryForm):
-    """ All brokers must have a form which will be used to construct and save queries
-    to the broker. They should subclass `BaseQueryForm` which includes some required
-    fields and contains logic for serializing and persisting the query parameters to the
-    database. This test form will only have one field.
+    """ A DataService must implement a form in order to be displayed in a TOM.
+    They should subclass `BaseQueryForm`. This test form will only have one field.
     """
     name = forms.CharField(required=True)
 
@@ -25,27 +26,71 @@ class TestDataService(BaseDataService):
         if term == 'notfound':
             raise MissingDataException
         self.query_results = test_query_results
+        return
 
     def get_form_class(self):
         return TestDataServiceForm
 
-    def pre_query_validation(self, query_parameters):
-        pass
+    def create_target_from_query(self, query_results, **kwargs):
+        return Target(**query_results)
 
-    def to_target(self, query_results):
-        target = super().to_target(query_results)
-        target.name = query_results['name']
-        target.type = query_results['type']
-        target.ra = query_results['ra']
-        target.dec = query_results['dec']
-        return target
+
+class EmptyTestDataService(BaseDataService):
+    name = 'TEST'
+    service_notes = "This is a test DataService."
+
+    def query_service(self, term):
+        if term == 'notfound':
+            raise MissingDataException
+        self.query_results = test_query_results
+        return
 
 
 class TestDataServiceClass(TestCase):
     """
-    Test the functionality of the TestDataService.
+    Test the functionality of the DataService class via the TestDataService.
     """
+    def test_query_service(self):
+        new_test_query = TestDataService()
+        self.assertEqual(new_test_query.query_results,{})
+        new_test_query.query_service('mytarget')
+        self.assertEqual(new_test_query.query_results, test_query_results)
 
     def test_to_target(self):
-        target = TestDataService.to_target(self, test_query_results)
+        new_test_query = TestDataService()
+        # Show to_target() returns error with no query_results
+        with self.assertRaises(MissingDataException):
+            new_test_query.to_target()
+        # Show to_target() works with the default query_results
+        new_test_query.query_targets('mytarget')
+        target = new_test_query.to_target()
         self.assertEqual(target.name, test_query_results['name'])
+        # Show to_target() works independently of the query.
+        new_test_query_results = test_query_results.copy()
+        new_test_query_results['name'] = 'target2'
+        target2 = new_test_query.to_target(new_test_query_results)
+        self.assertEqual(target2.name, 'target2')
+
+
+class TestUnimplementedDataServiceClass(TestCase):
+    """
+    Test the functionality of a DataService with unimplemented methods.
+    """
+
+    def test_no_create_data_product_from_query(self):
+        new_test_query = EmptyTestDataService()
+        # Show to_data_product() returns error when create_data_product_from_query undefined
+        with self.assertRaises(NotImplementedError):
+            new_test_query.to_data_product(test_query_results)
+
+    def test_no_create_target_from_query(self):
+        new_test_query = EmptyTestDataService()
+        # Show to_data_product() returns error when create_data_product_from_query undefined
+        with self.assertRaises(NotImplementedError):
+            new_test_query.to_target(test_query_results)
+
+    def test_no_create_reduced_datums_from_query(self):
+        new_test_query = EmptyTestDataService()
+        # Show to_data_product() returns error when create_data_product_from_query undefined
+        with self.assertRaises(NotImplementedError):
+            new_test_query.to_reduced_datums(test_query_results)
