ce93c099a8539c30ed33f42fda890b000949e5be
Joey Chatelain <jchate6@gmail.com>
2025-10-20 13:12:01 -0700

    update model doc strings

--- tom_dataservices/models.py
+++ tom_dataservices/models.py
@@ -3,26 +3,15 @@ from django.db import models
 
 class DataServiceQuery(models.Model):
     """
-    Class representing a query to a broker in a TOM
-
-    :param name: The user-given name of this query.
-    :type name: str
-
-    :param data_service: The name of the data service--this should come directly from the ``name`` attribute of
-        the DataService class representing the data service.
-    :type data_service: str
-
-    :param parameters: Parameters for this ``BrokerQuery``, stored as a JSON string.
-    :type parameters: dict
-
-    :param created: The time at which this ``BrokerQuery`` was created in the TOM database.
-    :type created: datetime
-
-    :param modified: The time at which this ``BrokerQuery`` was changed in the TOM database.
-    :type modified: datetime
-
-    :param last_run: The time at which this ``BrokerQuery`` was last run against its corresponding broker.
-    :type last_run: datetime
+    Class representing a query to a Data Service in a TOM
+    Fields:
+        name: The user-given name of this query.
+        data_service: The name of the data service--this should come directly from the ``name`` attribute of
+           the DataService class representing the data service.
+        parameters: Parameters for this ``DataServiceQuery``, stored as a JSON string.
+        created: The time at which this ``DataServiceQuery`` was created in the TOM database.
+        modified: The time at which this ``DataServiceQuery`` was changed in the TOM database.
+        last_run: The time at which this ``DataServiceQuery`` was last run against its corresponding broker.
     """
     name = models.CharField(max_length=500)
     data_service = models.CharField(max_length=50)
