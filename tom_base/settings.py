5254733b972da5d598000a88d62168775f3db2de
William (Lindy) Lindstrom <llindstrom@lco.global>
2026-01-12 15:22:16 -0800

    add django_tables2 to the test harness settings.py

--- tom_base/settings.py
+++ tom_base/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
+    'django_tables2',
     'django_gravatar',
     'django_htmx',
     'tom_targets',
