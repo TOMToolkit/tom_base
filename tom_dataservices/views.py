53bbba3e6a97fe8191f1522aff236c3263cf234a
Joey Chatelain <jchate6@gmail.com>
2026-01-26 14:31:23 -0700

    actually fix linting

--- tom_dataservices/views.py
+++ tom_dataservices/views.py
@@ -175,7 +175,7 @@ class RunQueryView(TemplateView):
                 query_parameters = data_service_class.build_query_parameters(input_parameters)
             # Check cached query is the same and pull cache if needed.
             if query_parameters == cache.get('query_params'):
-                cached_results = cache.get_many([f'result_{result_id}' for result_id in range(0,99)])
+                cached_results = cache.get_many([f'result_{result_id}' for result_id in range(0, 99)])
             if cached_results:
                 results = [cached_results[key] for key in cached_results]
             else:
