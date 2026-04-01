7c41824b3fb5565cad109c190867dbf422b02c7f
Joey Chatelain <jchate6@gmail.com>
2026-04-01 16:44:24 -0700

    fix tests for aliases

--- tom_dataservices/tests/data_services/test_mpc.py
+++ tom_dataservices/tests/data_services/test_mpc.py
@@ -13,6 +13,8 @@ class TestMPCExplorerDataService(TestCase):
         test_json_fp = files('tom_catalogs.tests.harvesters.data').joinpath('test_65803_mpc_orb.json')
         test_json = json.loads(test_json_fp.read_text())
         self.ds.query_results = [{'mpc_orb': test_json},]
+        self.target_result = self.ds.query_results[0]
+        self.target_result['name'] =  '65803'
 
     @patch('requests.get')
     def test_query_failure_no_object(self, mock_get):
@@ -27,13 +29,14 @@ class TestMPCExplorerDataService(TestCase):
         self.assertEqual(self.ds.query_results, {})
 
     def test_to_target(self):
-        target = self.ds.to_target(self.ds.query_results[0])
-        target.save(names=getattr(target, 'extra_names', []))
+        self.target_result['name'], self.target_result['aliases'] = self.ds.get_additional_mpc_names(
+            self.target_result['mpc_orb'])
+        target = self.ds.to_target(self.target_result)
         self.assertEqual(target.type, 'NON_SIDEREAL')
         self.assertEqual(target.scheme, 'MPC_MINOR_PLANET')
         self.assertEqual(target.name, '65803')
         self.assertEqual(target.names, ['65803', 'Didymos', '1996 GT'])
-        orbit_data = self.ds.query_results[0]['mpc_orb']
+        orbit_data = self.target_result['mpc_orb']
         elements = orbit_data['COM']['coefficient_values']
         self.assertEqual(target.epoch_of_elements, orbit_data['epoch_data']['epoch'])
         self.assertEqual(target.perihdist, elements[0])
@@ -54,15 +57,16 @@ class TestMPCExplorerDataService(TestCase):
 
     def test_to_target_no_name(self):
         # Modify designation data to one with a provisional id only
-        self.ds.query_results[0]['mpc_orb']['designation_data']['iau_name'] = ""
-        del (self.ds.query_results[0]['mpc_orb']['designation_data']['name'])
-        self.ds.query_results[0]['mpc_orb']['designation_data']['orbfit_name'] = "2025AA"
-        self.ds.query_results[0]['mpc_orb']['designation_data']['unpacked_primary_provisional_designation'] = \
+        self.target_result['mpc_orb']['designation_data']['iau_name'] = ""
+        del (self.target_result['mpc_orb']['designation_data']['name'])
+        self.target_result['mpc_orb']['designation_data']['orbfit_name'] = "2025AA"
+        self.target_result['mpc_orb']['designation_data']['unpacked_primary_provisional_designation'] = \
             "2025 AA"
-        self.ds.query_results[0]['mpc_orb']['designation_data']['unpacked_secondary_provisional_designations'] = []
+        self.target_result['mpc_orb']['designation_data']['unpacked_secondary_provisional_designations'] = []
+        self.target_result['name'], self.target_result['aliases'] = self.ds.get_additional_mpc_names(
+            self.target_result['mpc_orb'])
 
-        target = self.ds.to_target(self.ds.query_results[0])
-        target.save(names=getattr(target, 'extra_names', []))
+        target = self.ds.to_target(self.target_result)
         self.assertEqual(target.type, 'NON_SIDEREAL')
         self.assertEqual(target.scheme, 'MPC_MINOR_PLANET')
         self.assertEqual(target.name, '2025 AA')
@@ -70,19 +74,20 @@ class TestMPCExplorerDataService(TestCase):
 
     def test_to_target_multiple_alternative_desigs(self):
         # Modify designation data to one with a provisional id only
-        self.ds.query_results[0]['mpc_orb']['designation_data']['iau_name'] = ""
-        self.ds.query_results[0]['mpc_orb']['designation_data']['name'] = 'Fringilla'
-        self.ds.query_results[0]['mpc_orb']['designation_data']['orbfit_name'] = "709"
-        self.ds.query_results[0]['mpc_orb']['designation_data']['unpacked_primary_provisional_designation'] = \
+        self.target_result['mpc_orb']['designation_data']['iau_name'] = ""
+        self.target_result['mpc_orb']['designation_data']['name'] = 'Fringilla'
+        self.target_result['mpc_orb']['designation_data']['orbfit_name'] = "709"
+        self.target_result['mpc_orb']['designation_data']['unpacked_primary_provisional_designation'] = \
             "A911 CC"
-        self.ds.query_results[0]['mpc_orb']['designation_data']['unpacked_secondary_provisional_designations'] = \
+        self.target_result['mpc_orb']['designation_data']['unpacked_secondary_provisional_designations'] = \
             [
                 "A906 DA",
                 "1948 PK1",
                 "1956 CA"
             ]
-        target = self.ds.to_target(self.ds.query_results[0])
-        target.save(names=getattr(target, 'extra_names', []))
+        self.target_result['name'], self.target_result['aliases'] = self.ds.get_additional_mpc_names(
+            self.target_result['mpc_orb'])
+        target = self.ds.to_target(self.target_result)
         self.assertEqual(target.type, 'NON_SIDEREAL')
         self.assertEqual(target.scheme, 'MPC_MINOR_PLANET')
         self.assertEqual(target.name, '709')
@@ -90,15 +95,16 @@ class TestMPCExplorerDataService(TestCase):
 
     def test_comet_to_target(self):
         # Make fake parabolic comet
-        self.ds.query_results[0]['mpc_orb']['COM']['coefficient_values'][1] = 1.0
-        self.ds.query_results[0]['mpc_orb']['categorization']['object_type_int'] = 10
+        target_result = self.target_result
+        target_result['mpc_orb']['COM']['coefficient_values'][1] = 1.0
+        target_result['mpc_orb']['categorization']['object_type_int'] = 10
+        target_result['name'] = '65803'
 
-        target = self.ds.to_target(self.ds.query_results[0])
-        target.save(names=getattr(target, 'extra_names', []))
+        target = self.ds.to_target(target_result)
         self.assertEqual(target.type, 'NON_SIDEREAL')
         self.assertEqual(target.scheme, 'MPC_COMET')
         self.assertEqual(target.name, '65803')
-        orbit_data = self.ds.query_results[0]['mpc_orb']
+        orbit_data = target_result['mpc_orb']
         elements = orbit_data['COM']['coefficient_values']
         self.assertEqual(target.epoch_of_elements, orbit_data['epoch_data']['epoch'])
         self.assertEqual(target.perihdist, elements[0])
@@ -117,15 +123,14 @@ class TestMPCExplorerDataService(TestCase):
 
     def test_comet_to_target2(self):
         # Make fake near parabolic comet of right type
-        self.ds.query_results[0]['mpc_orb']['COM']['coefficient_values'][1] = 0.999
-        self.ds.query_results[0]['mpc_orb']['categorization']['object_type_int'] = 11
+        self.target_result['mpc_orb']['COM']['coefficient_values'][1] = 0.999
+        self.target_result['mpc_orb']['categorization']['object_type_int'] = 11
 
-        target = self.ds.to_target(self.ds.query_results[0])
-        target.save(names=getattr(target, 'extra_names', []))
+        target = self.ds.to_target(self.target_result)
         self.assertEqual(target.type, 'NON_SIDEREAL')
         self.assertEqual(target.scheme, 'MPC_COMET')
         self.assertEqual(target.name, '65803')
-        orbit_data = self.ds.query_results[0]['mpc_orb']
+        orbit_data = self.target_result['mpc_orb']
         elements = orbit_data['COM']['coefficient_values']
         self.assertEqual(target.epoch_of_elements, orbit_data['epoch_data']['epoch'])
         self.assertEqual(target.perihdist, elements[0])
@@ -152,8 +157,7 @@ class TestMPCExplorerDataServiceCanary(TestCase):
         query_parameters = {'desig': '433'}
         self.ds.query_service(query_parameters)
         self.ds.query_targets(query_parameters)
-        target = self.ds.to_target(self.ds.query_results[0])
-        target.save(names=getattr(target, 'extra_names', []))
+        target = self.ds.to_target(self.target_result)
         # Only test things that are not likely to change (much) with time
         self.assertEqual(target.name, '433')
         self.assertEqual(target.names, ['433', 'Eros', 'A898 PA', '1956 PC'])
@@ -171,7 +175,7 @@ class TestMPCExplorerDataServiceCanary(TestCase):
         query_parameters = {'desig': 'C/1995 O1'}
         self.ds.query_service(query_parameters)
         self.ds.query_targets(query_parameters)
-        target = self.ds.to_target(self.ds.query_results[0])
+        target = self.ds.to_target(self.target_result)
         target.save(names=getattr(target, 'extra_names', []))
         # Only test things that are not likely to change (much) with time
         self.assertEqual(target.name, 'C/1995 O1')
