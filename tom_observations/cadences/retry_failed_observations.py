666c48d6185e3c547e94ad9147d4b9d082d93a89
Moira Andrews <moira.lee.andrews@gmail.com>
2026-04-01 17:23:10 -0400

    updated logic and handling to be more direct and have runcadencestrategies display expected response

--- tom_observations/cadences/retry_failed_observations.py
+++ tom_observations/cadences/retry_failed_observations.py
@@ -1,9 +1,12 @@
 from datetime import timedelta
 from dateutil.parser import parse
+import logging
 
 from tom_observations.cadence import BaseCadenceForm, CadenceStrategy
-from tom_observations.models import ObservationRecord
+from tom_observations.models import ObservationRecord, DynamicCadence
 from tom_observations.facility import get_service_class
+
+logger = logging.getLogger(__name__)
 
 
 class RetryFailedObservationsForm(BaseCadenceForm):
@@ -31,51 +34,52 @@ class RetryFailedObservationsStrategy(CadenceStrategy):
 
         facility_class = get_service_class(last_obs.facility)
         facility = facility_class()
+        start_keyword, end_keyword = facility.get_start_end_keywords()
         facility.update_observation_status(last_obs.observation_id)
         last_obs.refresh_from_db()
 
-        if not last_obs.terminal:
+        if not last_obs.terminal: #observation is still pending, do nothing
             return
-        elif last_obs.status == 'COMPLETED':
+        
+        elif not last_obs.failed: #observation succeeded
             self.dynamic_cadence.active = False
             self.dynamic_cadence.save()
-            return
+            return 'COMPLETED'
 
-        if not last_obs.failed:
-            return
+        else: #observation failed, submit a new one
+            observation_payload = last_obs.parameters.copy()
 
-        observation_payload = last_obs.parameters.copy()
-
-        start_keyword, end_keyword = facility.get_start_end_keywords()
-        observation_payload = self.advance_window(
-            observation_payload, start_keyword=start_keyword, end_keyword=end_keyword
-        )
-
-        obs_type = observation_payload.get('observation_type')
-        form = facility.get_form(obs_type)(observation_payload)
-
-        if not form.is_valid():
-            return
-
-        observation_ids = facility.submit_observation(form.observation_payload())
-        new_observations = []
-
-        for observation_id in observation_ids:
-            record = ObservationRecord.objects.create(
-                target=last_obs.target,
-                facility=facility.name,
-                parameters=observation_payload,
-                observation_id=observation_id
+            observation_payload = self.advance_window(
+                observation_payload, start_keyword=start_keyword, end_keyword=end_keyword
             )
-            self.dynamic_cadence.observation_group.observation_records.add(record)
-            new_observations.append(record)
+                       
+            obs_type = observation_payload.get('observation_type')
+            form = facility.get_form(obs_type)(observation_payload)
+            
+            if not form.is_valid():
+                logger.error(msg=f'Unable to submit next cadenced observation: {form.errors}')
+                raise Exception(f'Unable to submit next cadenced observation: {form.errors}')
 
-        self.dynamic_cadence.observation_group.save()
+            observation_ids = facility.submit_observation(form.observation_payload())
+            new_observations = []
+        
+            for observation_id in observation_ids:
+                record = ObservationRecord.objects.create(
+                    target=last_obs.target,
+                    facility=facility.name,
+                    parameters=observation_payload,
+                    observation_id=observation_id
+                )
+                self.dynamic_cadence.observation_group.observation_records.add(record)
+                new_observations.append(record)
 
-        for obsr in new_observations:
-            facility.update_observation_status(obsr.observation_id)
+            self.dynamic_cadence.observation_group.save()
 
-        return new_observations
+            for obsr in new_observations:
+                facility.update_observation_status(obsr.observation_id)
+                obsr.refresh_from_db()
+
+            return new_observations
 
     def advance_window(self, observation_payload, start_keyword='start', end_keyword='end'):
         cadence_frequency = self.dynamic_cadence.cadence_parameters.get('cadence_frequency')
