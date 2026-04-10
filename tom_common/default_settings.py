094f3336a3265fcff2b8db6742de77136608de7d
Joey Chatelain <jchate6@gmail.com>
2026-04-10 14:47:10 -0700

    add default_settings

---
+++ tom_common/default_settings.py
@@ -0,0 +1,54 @@
+"""
+Default settings for TOM Toolkits.
+This sets all of the mandatory apps and middleware as defaults 
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/4.2/ref/settings/
+"""
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django_extensions',
+    'django_tasks',
+    'django_tasks.backends.database',
+    'guardian',
+    'tom_common',
+    'django_comments',
+    'bootstrap4',
+    'crispy_bootstrap4',
+    'crispy_forms',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'django_tables2',
+    'django_gravatar',
+    'django_htmx',
+    'tom_targets',
+    'tom_alerts',
+    'tom_catalogs',
+    'tom_observations',
+    'tom_dataproducts',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
+    'tom_common.middleware.Raise403Middleware',
+    'tom_common.middleware.ExternalServiceMiddleware',
+    'tom_common.middleware.AuthStrategyMiddleware',
+]
