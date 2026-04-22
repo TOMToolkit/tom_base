9b675cd3887bb2c108cb6c5d9c305256fa349252
Austin Riba <austin@m51.io>
2026-04-22 15:40:27 -0700

    Fire reduced datum post_save for all concrete types

--- tom_targets/signals/handlers.py
+++ tom_targets/signals/handlers.py
@@ -2,17 +2,20 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 from guardian.utils import clean_orphan_obj_perms
 
-from tom_dataproducts.models import ReducedDatum
+from tom_dataproducts.models import REDUCED_DATUM_MODELS
 from tom_targets.sharing import continuous_share_data
 from tom_targets.models import Target
 
 
-@receiver(post_save, sender=ReducedDatum)
 def cb_reduceddatum_post_save(sender, instance, *args, **kwargs):
     # When a new ReducedDatum is created or updated, check for any persistentshare instances on that target
     # and if they exist, attempt to share the new data
     target = instance.target
     continuous_share_data(target, reduced_datums=[instance])
+
+
+for model in REDUCED_DATUM_MODELS:
+    post_save.connect(cb_reduceddatum_post_save, sender=model)
 
 
 @receiver(post_delete, sender=Target)
