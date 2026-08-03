0d8fd18c479f333ab8197e4dc4db90e3d7e061fd
Joey Chatelain <jchate6@gmail.com>
2026-08-03 14:00:31 -0700

    Revert "no longer need special unique constraints for duplicate data because standard constraints are sufficient"
    This reverts commit aee1f5c20eb85af2e03af6fdb158c26177cafbea.

--- tom_dataproducts/models.py
+++ tom_dataproducts/models.py
@@ -450,6 +450,13 @@ class PhotometryReducedDatum(ReducedDatumCommon):
     bandpass = models.CharField(max_length=32)
     exposure_time = models.FloatField(blank=True, null=True)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["target", "bandpass", "timestamp"], name="unique_photometry"
+            )
+        ]
+
 
 class SpectroscopyReducedDatum(ReducedDatumCommon):
     setup = models.CharField(max_length=2000, blank=True, default="")
@@ -460,6 +467,14 @@ class SpectroscopyReducedDatum(ReducedDatumCommon):
     flux_unit = models.TextField(blank=True, default="")
     wavelength_unit = models.TextField(blank=True, default="")
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["target", "timestamp", "telescope", "instrument"],
+                name="unique_spectroscopy",
+            )
+        ]
+
 
 class AstrometryReducedDatum(ReducedDatumCommon):
     ra = models.FloatField()
@@ -468,6 +483,14 @@ class AstrometryReducedDatum(ReducedDatumCommon):
     dec_error = models.FloatField(null=True, blank=True, default=None)
     ra_error_units = models.CharField(max_length=32, blank=True, default="")
     dec_error_units = models.CharField(max_length=32, blank=True, default="")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["target", "timestamp", "telescope", "instrument"],
+                name="unique_astrometry",
+            )
+        ]
 
 
 REDUCED_DATUM_MODELS = (
